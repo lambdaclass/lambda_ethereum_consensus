75b5b86f70cdf732f03eb2bfc4e98ac2d3de316c
Tomás Arjovsky <t.arjovsky@gmail.com>
2023-07-27 20:48:27 +0200

    project setup (#8)

---
+++ lib/lambda_ethereum_consensus/application.ex
@@ -0,0 +1,20 @@
+defmodule LambdaEthereumConsensus.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: LambdaEthereumConsensus.Worker.start_link(arg)
+      # {LambdaEthereumConsensus.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: LambdaEthereumConsensus.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
