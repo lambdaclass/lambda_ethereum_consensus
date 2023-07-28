99ea9c252da4c6591da63c26b7afcd9f497aad0d
Tomá <47506558+MegaRedHand@users.noreply.github.com>
2023-07-28 11:13:21 -0300

    Use macro for C function signature

--- libp2p/libp2p.c
+++ libp2p/libp2p.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <erl_nif.h>
 
-static ERL_NIF_TERM hello(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
+#define ERL_FUNCTION(FUNCTION_NAME) static ERL_NIF_TERM FUNCTION_NAME(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
+
+ERL_FUNCTION(hello)
 {
     return enif_make_atom(env, "world");
 }
 
-static ERL_NIF_TERM my_function(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
+ERL_FUNCTION(my_function)
 {
     int a, b;
     enif_get_int(env, argv[0], &a);
@@ -17,7 +19,7 @@ static ERL_NIF_TERM my_function(ErlNifEnv *env, int argc, const ERL_NIF_TERM arg
     return enif_make_int(env, result);
 }
 
-static ERL_NIF_TERM host_new(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
+ERL_FUNCTION(host_new)
 {
     int result = New();
     if (result == 0)
@@ -27,7 +29,7 @@ static ERL_NIF_TERM host_new(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]
     return enif_make_tuple2(env, enif_make_atom(env, "ok"), enif_make_uint64(env, result));
 }
 
-static ERL_NIF_TERM host_close(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
+ERL_FUNCTION(host_close)
 {
     uintptr_t handle;
     enif_get_uint64(env, argv[0], &handle);
@@ -40,8 +42,8 @@ static ERL_NIF_TERM host_close(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv
 static ErlNifFunc nif_funcs[] = {
     {"hello", 0, hello},
     {"my_function", 2, my_function},
-    {"host_close", 1, host_close},
     {"host_new", 0, host_new},
+    {"host_close", 1, host_close},
 };
 
 ERL_NIF_INIT(Elixir.Libp2p, nif_funcs, NULL, NULL, NULL, NULL)
