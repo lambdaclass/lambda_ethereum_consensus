34fac42e970ffaa0743a84e6413539b20160db6b
Tomá <47506558+MegaRedHand@users.noreply.github.com>
2023-07-31 17:27:30 -0300

    Add integration test for bindings

--- test/libp2p_test.exs
+++ test/libp2p_test.exs
@@ -33,15 +33,56 @@ defmodule Libp2pTest do
     Libp2p.host_close(host)
   end
 
-  # test "Start two hosts, and play one round of ping-pong" do
-  #   # Setup sender
-  #   {:ok, sender} = Libp2p.host_new()
-  #   # Setup receiver
-  #   {:ok, recver} = Libp2p.host_new()
+  test "Start two hosts, and play one round of ping-pong" do
+    # Setup sender
+    {:ok, sender} = Libp2p.host_new()
+    # Setup receiver
+    {:ok, recver} = Libp2p.host_new()
 
-  #   :ok = Libp2p.host_set_stream_handler(recver, "/pong")
+    # (recver) Set stream handler
+    :ok = Libp2p.host_set_stream_handler(recver, "/pong")
 
-  #   :ok = Libp2p.host_close(host1)
-  #   :ok = Libp2p.host_close(host2)
-  # end
+    # (sender) Add recver address to peerstore
+    {:ok, peerstore} = Libp2p.host_peerstore(sender)
+    {:ok, id} = Libp2p.host_id(recver)
+    {:ok, addrs} = Libp2p.host_addrs(recver)
+
+    Libp2p.peerstore_add_addrs(
+      peerstore,
+      id,
+      addrs,
+      2_512_512
+    )
+
+    # (sender) Create stream sender -> recver
+    {:ok, send} = Libp2p.host_new_stream(sender, id, "/pong")
+
+    # (sender) Write "ping" to stream
+    {:ok, 4} = Libp2p.stream_write(send, "ping")
+
+    # (recver) Receive the stream via the configured stream handler
+    {:ok, recv} =
+      receive do
+        msg -> msg
+      after
+        1_000 -> :timeout
+      end
+
+    # (recver) Read the "ping" message from the stream
+    {:ok, "ping"} = Libp2p.stream_read(recv)
+
+    # (recver) Write "pong" to the stream
+    {:ok, 4} = Libp2p.stream_write(recv, "pong")
+
+    # (sender) Read the "pong" message from the stream
+    {:ok, "pong"} = Libp2p.stream_read(send)
+
+    # Close both streams
+    Libp2p.stream_close(send)
+    Libp2p.stream_close(recv)
+
+    # Close both hosts
+    Libp2p.host_close(sender)
+    Libp2p.host_close(recver)
+  end
 end
