433a75e041476a4e02b1fdfb5445eb28e3d6dd9f
Martin Paulucci <martin.c.paulucci@gmail.com>
2023-08-03 16:28:03 +0200

    build: add `.tool-versions`. (#18)
    * Add `.tool-versions`.
    
    * Go 1.20.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LambdaEthereumConsensus.MixProject do
     [
       app: :lambda_ethereum_consensus,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
