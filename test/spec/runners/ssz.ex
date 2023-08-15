be7f4b4b49d9201a263a4bed67717425ae2830fc
Tomás <47506558+MegaRedHand@users.noreply.github.com>
2023-08-15 17:39:03 -0300

    test: add ability to run EF spec tests (#32)
    * Add Makefile to download test vectors
    
    * Add example test using vector data (skipped)
    
    * Generate tests dynamically
    
    * Fix warning in libp2p_test
    
    * Rename dir in gitignore
    
    * Make test runner selection easier to change
    
    * Add Makefile for downloading test vectors
    
    * Add tags to tests
    
    * Comment about test filtering
    
    * Merge makefile with root makefile
    
    * Move spec tests and vectors to another dir
    
    * Revert move, and fix credo warnings
    
    * Use variable instead of module attribute
    
    * Un-skip implemented tests
    
    * Split tests in modules
    
    * Fix warning
    
    * Add spec-test running to the CI
    
    * Uncompress vectors in CI
    
    * Make download-spectests cache lookup-only
    
    * Rename test/spec-test -> test/spec
    
    * Add customizable case skipping in runners
    
    * Run only "minimal" spec-tests
    
    * Rename ssz runner file
    
    * Use compiled test files (for spectests)
    
    * Fix credo lints
    
    * Fix dialyzer warnings
    
    * Re-skip SSZTestRunner tests

---
+++ test/spec/runners/ssz.ex
@@ -0,0 +1,35 @@
+defmodule SSZTestRunner do
+  use ExUnit.CaseTemplate
+
+  @moduledoc """
+  Runner for SSZ test cases. `run_test_case/1` is the main entrypoint.
+  """
+
+  @doc """
+  Returns true if the given testcase should be skipped
+  """
+  def skip?(_testcase) do
+    # add SSZ test case skipping here
+    true
+  end
+
+  @doc """
+  Runs the given test case.
+  """
+  def run_test_case(testcase = %SpecTestCase{}) do
+    case_dir = SpecTestCase.dir(testcase)
+
+    compressed = File.read!(case_dir <> "/serialized.ssz_snappy")
+    assert {:ok, decompressed} = :snappyer.decompress(compressed)
+
+    expected = YamlElixir.read_from_file!(case_dir <> "/value.yaml")
+    expected_root = YamlElixir.read_from_file!(case_dir <> "/roots.yaml")
+
+    assert_ssz(decompressed, expected, expected_root)
+  end
+
+  defp assert_ssz(_serialized, _expected, _expected_root) do
+    # add SSZ comparison here
+    assert false
+  end
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,13 @@
 ExUnit.start()
+
+# Load all modules as ExUnit tests (needed because we use .ex files)
+# Copied from https://github.com/elixir-lang/elixir/issues/10983#issuecomment-1133554155
+for module <- Application.spec(Mix.Project.config()[:app], :modules) do
+  ex_unit = Keyword.get(module.module_info(:attributes), :ex_unit_async, [])
+
+  cond do
+    true in ex_unit -> ExUnit.Server.add_async_module(module)
+    false in ex_unit -> ExUnit.Server.add_sync_module(module)
+    true -> :ok
+  end
+end
