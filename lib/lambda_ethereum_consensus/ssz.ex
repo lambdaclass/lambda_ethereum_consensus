bcccec00575bd90a00de081869ea76346abcc3cb
Martin Paulucci <martin.paulucci@lambdaclass.com>
2023-08-16 15:57:10 +0200

    Fix credo.

--- lib/lambda_ethereum_consensus/ssz.ex
+++ lib/lambda_ethereum_consensus/ssz.ex
@@ -1,11 +1,15 @@
 defmodule LambdaEthereumConsensus.Ssz do
+  @moduledoc """
+  SimpleSerialize (SSZ) serialization and deserialization.
+  """
   use Rustler, otp_app: :lambda_ethereum_consensus, crate: "ssz_nif"
 
-  @spec to_ssz(map) :: binary
+  @spec to_ssz(map) :: binary | :error
   def to_ssz(_map), do: error()
 
-  @spec from_ssz(binary) :: map
+  @spec from_ssz(binary) :: map | :error
   def from_ssz(_bin), do: error()
 
-  defp error(), do: :erlang.nif_error(:nif_not_loaded)
+  @spec error() :: :error
+  defp error, do: :erlang.nif_error(:nif_not_loaded)
 end
