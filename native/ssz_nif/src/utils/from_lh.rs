d0872cd4b95548be044f149bd74f0634514547ff
Tomás <47506558+MegaRedHand@users.noreply.github.com>
2023-08-17 12:00:51 -0300

    refactor(ssz): reduce SSZ boilerplate by using macros (#39)
    * Use macros to reduce dispatch boilerplate
    
    * Use macros to produce same code
    
    * Clean up the crate structure a bit
    
    * Add `Fork` type
    
    * Add some documentation
    
    * Add `ForkData` type
    
    * Don't use `#[macro_export]`

---
+++ native/ssz_nif/src/utils/from_lh.rs
@@ -0,0 +1,32 @@
+use crate::utils::helpers::bytes_to_binary;
+use ethereum_types::H256;
+use lighthouse_types::Epoch;
+use rustler::Binary;
+
+pub(crate) trait FromLH<'a, T> {
+    fn from(value: T, env: rustler::Env<'a>) -> Self;
+}
+
+macro_rules! impl_int {
+    ($t:ty => $u:ty) => {
+        impl<'a> FromLH<'a, $t> for $u {
+            fn from(value: $t, _env: rustler::Env<'a>) -> Self {
+                value.into()
+            }
+        }
+    };
+}
+
+impl_int!(Epoch => u64);
+
+impl<'a> FromLH<'a, H256> for Binary<'a> {
+    fn from(value: H256, env: rustler::Env<'a>) -> Self {
+        bytes_to_binary(env, value.as_bytes())
+    }
+}
+
+impl<'a> FromLH<'a, [u8; 4]> for Binary<'a> {
+    fn from(value: [u8; 4], env: rustler::Env<'a>) -> Self {
+        bytes_to_binary(env, &value)
+    }
+}
