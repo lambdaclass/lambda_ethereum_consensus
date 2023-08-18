88b21d9f3fb1094e776086fb24b3df1041488be3
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2023-08-18 15:30:00 +0200

    fix for credo linting

--- lib/lambda_ethereum_consensus/jwt.ex
+++ lib/lambda_ethereum_consensus/jwt.ex
@@ -8,16 +8,16 @@ defmodule LambdaEthereumConsensus.JWT do
   # Following the specs here: https://github.com/ethereum/execution-apis/blob/main/src/engine/authentication.md
   # 3 properties are required for the JWT to be valid:
   # 1. The execution layer and consensus layer clients SHOULD accept a configuration parameter: jwt-secret,
-  # which designates a file containing the hex-encoded 256 bit secret key to be used for verifying/generating JWT tokens.
+  # which designates a file containing the hex-encoded 256 bit secret key to be used for verifying/generating JWT tokens
   # 2. The encoding algorithm used for the JWT generation must be HMAC + SHA256 (HS256)
   # 3. iat (issued-at) claim. The execution layer client SHOULD only accept iat timestamps
-  # which are within +-60 seconds from the current time.
+  # which are within +-60 seconds from the current time
 
   @doc """
   Generates a JWT token using HS256 algo and provided secret, additionaly adds an iat claim at current time.
   """
   @spec generate_token :: {:error, atom | keyword} | {:ok, binary, %{optional(binary) => any}}
-  def generate_token() do
+  def generate_token do
     claim = %{"iat" => Joken.current_time()}
 
     signer =
