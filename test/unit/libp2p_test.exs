1a4b27af72a57bbf080b3e67baa17b9915f41d86
Tomá <47506558+MegaRedHand@users.noreply.github.com>
2023-08-24 17:22:40 -0300

    Add CL GossipSub example

--- test/unit/libp2p_test.exs
+++ test/unit/libp2p_test.exs
@@ -164,7 +164,7 @@ defmodule Libp2pTest do
     end)
 
     # (sender) Give a head start to the other process
-    Process.sleep(1)
+    Process.sleep(100)
 
     # (sender) Publish a message to the topic
     :ok = Libp2p.topic_publish(topic_sender, msg)
@@ -187,7 +187,9 @@ defmodule Libp2pTest do
       {:ok, id} = Libp2p.node_id(node)
       {:ok, addrs} = Libp2p.node_multiaddr(node)
 
-      :ok = Libp2p.peerstore_add_addrs(peerstore, id, addrs, Libp2p.ttl_permanent_addr())
+      # 1 minute
+      ttl = 6 * 10 ** 10
+      :ok = Libp2p.peerstore_add_addrs(peerstore, id, addrs, ttl)
 
       fun.(id)
     end
@@ -226,7 +228,7 @@ defmodule Libp2pTest do
     :ok = Libp2p.host_close(host)
   end
 
-  # @tag :skip
+  @tag :skip
   @tag timeout: :infinity
   test "ping peers" do
     {:ok, host} = Libp2p.host_new()
@@ -253,6 +255,51 @@ defmodule Libp2pTest do
         {:error, err} -> IO.puts(err)
       end
     end)
+
+    :ok = Libp2p.host_close(host)
+  end
+
+  def read_gossip_msg(sub) do
+    case Libp2p.subscription_next(sub) do
+      {:ok, msg} ->
+        # NOTE: gossip messages are Snappy-compressed with BLOCK format (not frame)
+        msg
+        |> Libp2p.message_data()
+        |> Base.encode16()
+        |> then(&IO.puts(["\"#{&1}\""]))
+
+      {:error, err} ->
+        IO.puts(err)
+    end
+
+    read_gossip_msg(sub)
+  end
+
+  @tag :skip
+  @tag timeout: :infinity
+  test "Gossip with CL peers" do
+    # Setup host
+    {:ok, host} = Libp2p.host_new()
+
+    # Create GossipSubs
+    {:ok, gsub} = Libp2p.new_gossip_sub(host)
+
+    # Topic for Mainnet, Capella fork, attestation subnet 0
+    topic_str = "/eth2/bba4da96/beacon_attestation_0/ssz_snappy"
+
+    # Join the topic
+    {:ok, topic} = Libp2p.pub_sub_join(gsub, topic_str)
+
+    # Subscribe to the topic
+    {:ok, sub} = Libp2p.topic_subscribe(topic)
+
+    # Start discovery in another process
+    {:ok, listener} = Libp2p.listen_v5("0.0.0.0:45124", @bootnodes)
+    {:ok, iterator} = Libp2p.listener_random_nodes(listener)
+    spawn(fn -> find_peers(host, iterator, &Libp2p.host_connect(host, &1)) end)
+
+    # Read gossip messages
+    read_gossip_msg(sub)
 
     :ok = Libp2p.host_close(host)
   end
