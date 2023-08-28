149925e1cff21100a1c11d00d9415b1f02a2cb34
Martin Paulucci <martin.paulucci@lambdaclass.com>
2023-08-28 20:01:57 +0200

    Remove lighthouse dep.

--- native/ssz_nif/src/utils/from_elx.rs
+++ native/ssz_nif/src/utils/from_elx.rs
@@ -1,7 +1,4 @@
 use ethereum_types::H256;
-use lighthouse_types::{
-    BitList, Epoch, FixedVector, PublicKeyBytes, SignatureBytes, Slot, Unsigned,
-};
 use rustler::Binary;
 use ssz::Decode;
 
@@ -18,9 +15,6 @@ macro_rules! trivial_impl {
         }
     };
 }
-
-trivial_impl!(u64 => Epoch);
-trivial_impl!(u64 => Slot);
 
 impl<T> FromElx<T> for T {
     fn from(value: Self) -> Self {
@@ -44,37 +38,5 @@ impl<'a> FromElx<Binary<'a>> for [u8; 4] {
     fn from(value: Binary) -> Self {
         // length is checked from the Elixir side
         value.as_slice().try_into().unwrap()
-    }
-}
-
-impl<'a> FromElx<Binary<'a>> for PublicKeyBytes {
-    fn from(value: Binary<'a>) -> Self {
-        // length is checked from the Elixir side
-        PublicKeyBytes::deserialize(value.as_slice()).unwrap()
-    }
-}
-
-impl<'a> FromElx<Binary<'a>> for SignatureBytes {
-    fn from(value: Binary<'a>) -> Self {
-        // length is checked from the Elixir side
-        SignatureBytes::deserialize(value.as_slice()).unwrap()
-    }
-}
-
-impl<'a, N: Unsigned> FromElx<Binary<'a>> for BitList<N> {
-    fn from(value: Binary<'a>) -> Self {
-        // TODO: remove unwrap?
-        Self::from_ssz_bytes(&value).unwrap()
-    }
-}
-
-impl<'a, Elx, Lh, N> FromElx<Vec<Elx>> for FixedVector<Lh, N>
-where
-    Lh: FromElx<Elx>,
-    N: Unsigned,
-{
-    fn from(value: Vec<Elx>) -> Self {
-        // TODO: remove unwrap?
-        Self::new(value.into_iter().map(FromElx::from).collect()).unwrap()
     }
 }
--- native/ssz_nif/src/utils/from_lh.rs
+++ native/ssz_nif/src/utils/from_lh.rs
@@ -1,8 +1,5 @@
 use crate::utils::helpers::bytes_to_binary;
 use ethereum_types::H256;
-use lighthouse_types::{
-    BitList, Epoch, FixedVector, PublicKeyBytes, SignatureBytes, Slot, Unsigned,
-};
 use rustler::Binary;
 
 pub(crate) trait FromLH<'a, T> {
@@ -18,9 +15,6 @@ macro_rules! trivial_impl {
         }
     };
 }
-
-trivial_impl!(Epoch => u64);
-trivial_impl!(Slot => u64);
 
 impl<'a, T> FromLH<'a, T> for T {
     fn from(value: Self, _env: rustler::Env<'a>) -> Self {
@@ -43,34 +37,5 @@ impl<'a> FromLH<'a, H256> for Binary<'a> {
 impl<'a> FromLH<'a, [u8; 4]> for Binary<'a> {
     fn from(value: [u8; 4], env: rustler::Env<'a>) -> Self {
         bytes_to_binary(env, &value)
-    }
-}
-
-impl<'a> FromLH<'a, PublicKeyBytes> for Binary<'a> {
-    fn from(value: PublicKeyBytes, env: rustler::Env<'a>) -> Self {
-        bytes_to_binary(env, value.as_serialized())
-    }
-}
-
-impl<'a> FromLH<'a, SignatureBytes> for Binary<'a> {
-    fn from(value: SignatureBytes, env: rustler::Env<'a>) -> Self {
-        bytes_to_binary(env, &value.serialize())
-    }
-}
-
-impl<'a, N: Unsigned> FromLH<'a, BitList<N>> for Binary<'a> {
-    fn from(value: BitList<N>, env: rustler::Env<'a>) -> Self {
-        bytes_to_binary(env, value.into_bytes().as_slice())
-    }
-}
-
-impl<'a, Elx, Lh, N> FromLH<'a, FixedVector<Lh, N>> for Vec<Elx>
-where
-    Elx: FromLH<'a, Lh> + ToOwned,
-    N: Unsigned,
-{
-    fn from(value: FixedVector<Lh, N>, env: rustler::Env<'a>) -> Self {
-        let v: Vec<_> = value.into();
-        v.into_iter().map(|x| Elx::from(x, env)).collect::<Vec<_>>()
     }
 }
