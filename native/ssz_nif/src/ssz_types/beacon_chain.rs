0fdec8aeb22a0d992ceb074b1e9b0b7bba018d71
Tomá <47506558+MegaRedHand@users.noreply.github.com>
2023-08-28 14:49:14 -0300

    Remove comment

--- native/ssz_nif/src/ssz_types/beacon_chain.rs
+++ native/ssz_nif/src/ssz_types/beacon_chain.rs
@@ -1,6 +1,3 @@
-//! This module re-exports all the types from [`lighthouse_types`].
-//! It also adds some aliases for types that receive generic arguments.
-
 use ssz_derive::{Decode, Encode};
 
 type Byte32 = [u8; 32];
