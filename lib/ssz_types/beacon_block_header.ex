953ff4ebdf38ff6b1cdc7a902b1b1a5a69aaf0ce
Akash SM <akashsm@ce.iitr.ac.in>
2023-08-28 23:58:34 +0530

    fix: lint issue

--- lib/ssz_types/beacon_block_header.ex
+++ lib/ssz_types/beacon_block_header.ex
@@ -9,17 +9,17 @@ defmodule SszTypes.BeaconBlockHeader do
     :proposer_index,
     :parent_root,
     :state_root,
-    :body_root,
+    :body_root
   ]
 
   @enforce_keys fields
   defstruct fields
 
   @type t :: %__MODULE__{
-    slot: SszTypes.slot(),
-    proposer_index: SszTypes.validator_index(),
-    parent_root: SszTypes.root(),
-    state_root: SszTypes.root(),
-    body_root: SszTypes.root(),
-  }
+          slot: SszTypes.slot(),
+          proposer_index: SszTypes.validator_index(),
+          parent_root: SszTypes.root(),
+          state_root: SszTypes.root(),
+          body_root: SszTypes.root()
+        }
 end
