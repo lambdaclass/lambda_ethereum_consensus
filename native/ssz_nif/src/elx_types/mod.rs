e5b709a39df58ade4d4c628e6d72b103a9a76156
Martin Paulucci <martin.paulucci@lambdaclass.com>
2023-08-29 13:48:35 +0200

    remove legacy Lighthouse references from ssz implementation.

--- native/ssz_nif/src/elx_types/mod.rs
+++ native/ssz_nif/src/elx_types/mod.rs
@@ -2,7 +2,7 @@
 //!
 //! To add a new type, add the struct definition (with [`rustler`] types)
 //! in the corresponding module. You may need to add some [`FromElx`] and
-//! [`FromLH`] implementations to convert between the types.
+//! [`FromSsz`] implementations to convert between the types.
 
 mod beacon_chain;
 pub(crate) use beacon_chain::*;
--- native/ssz_nif/src/utils/from_ssz.rs
+++ native/ssz_nif/src/utils/from_ssz.rs
@@ -3,13 +3,13 @@ use rustler::Binary;
 use ssz::Encode;
 use ssz_types::{typenum::Unsigned, BitList, BitVector, FixedVector};
 
-pub(crate) trait FromLH<'a, T> {
+pub(crate) trait FromSsz<'a, T> {
     fn from(value: T, env: rustler::Env<'a>) -> Self;
 }
 
 macro_rules! trivial_impl {
     ($t:ty) => {
-        impl<'a> FromLH<'a, $t> for $t {
+        impl<'a> FromSsz<'a, $t> for $t {
             fn from(value: $t, _env: rustler::Env<'a>) -> Self {
                 value
             }
@@ -23,48 +23,48 @@ trivial_impl!(u16);
 trivial_impl!(u32);
 trivial_impl!(u64);
 
-impl<'a, const N: usize> FromLH<'a, [u8; N]> for Binary<'a> {
+impl<'a, const N: usize> FromSsz<'a, [u8; N]> for Binary<'a> {
     fn from(value: [u8; N], env: rustler::Env<'a>) -> Self {
         bytes_to_binary(env, &value)
     }
 }
 
-impl<'a, N: Unsigned> FromLH<'a, FixedVector<u8, N>> for Binary<'a> {
+impl<'a, N: Unsigned> FromSsz<'a, FixedVector<u8, N>> for Binary<'a> {
     fn from(value: FixedVector<u8, N>, env: rustler::Env<'a>) -> Self {
         bytes_to_binary(env, &value)
     }
 }
 
-impl<'a, Ssz, Elx, N> FromLH<'a, FixedVector<Ssz, N>> for Vec<Elx>
+impl<'a, Ssz, Elx, N> FromSsz<'a, FixedVector<Ssz, N>> for Vec<Elx>
 where
-    Elx: FromLH<'a, Ssz>,
+    Elx: FromSsz<'a, Ssz>,
     N: Unsigned,
 {
     fn from(value: FixedVector<Ssz, N>, env: rustler::Env<'a>) -> Self {
         let as_vec: Vec<_> = value.into();
         as_vec
             .into_iter()
-            .map(|v: Ssz| FromLH::from(v, env))
+            .map(|v: Ssz| FromSsz::from(v, env))
             .collect()
     }
 }
 
-impl<'a, Ssz, Elx> FromLH<'a, Vec<Ssz>> for Vec<Elx>
+impl<'a, Ssz, Elx> FromSsz<'a, Vec<Ssz>> for Vec<Elx>
 where
-    Elx: FromLH<'a, Ssz>,
+    Elx: FromSsz<'a, Ssz>,
 {
     fn from(value: Vec<Ssz>, env: rustler::Env<'a>) -> Self {
-        value.into_iter().map(|v| FromLH::from(v, env)).collect()
+        value.into_iter().map(|v| FromSsz::from(v, env)).collect()
     }
 }
 
-impl<'a, N: Unsigned> FromLH<'a, BitVector<N>> for Binary<'a> {
+impl<'a, N: Unsigned> FromSsz<'a, BitVector<N>> for Binary<'a> {
     fn from(value: BitVector<N>, env: rustler::Env<'a>) -> Self {
         bytes_to_binary(env, &value.as_ssz_bytes())
     }
 }
 
-impl<'a, N: Unsigned> FromLH<'a, BitList<N>> for Binary<'a> {
+impl<'a, N: Unsigned> FromSsz<'a, BitList<N>> for Binary<'a> {
     fn from(value: BitList<N>, env: rustler::Env<'a>) -> Self {
         bytes_to_binary(env, &value.as_ssz_bytes())
     }
