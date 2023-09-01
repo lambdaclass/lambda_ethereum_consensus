24840880bcce05e78e7e07e20518f0f274e57146
Paul-Henry Kajfasz <42912740+phklive@users.noreply.github.com>
2023-09-01 16:39:04 +0200

    feat: add ExecutionPayload & ExecutionPayloadHeader & Withdrawal types (#122)
    Co-authored-by: Tomá <47506558+MegaRedHand@users.noreply.github.com>

--- native/ssz_nif/src/ssz_types/mod.rs
+++ native/ssz_nif/src/ssz_types/mod.rs
@@ -4,9 +4,18 @@
 
 mod beacon_chain;
 
+pub(crate) use beacon_chain::*;
+use ssz_derive::{Decode, Encode};
+use ssz_types::{typenum, FixedVector, VariableList};
+
+// This type is a little-endian encoded uint256.
+// We use this to because of Erlang's NIF limitations.
+#[derive(Clone, Copy, Encode, Decode)]
+#[ssz(struct_behaviour = "transparent")]
+pub(crate) struct Uint256(pub(crate) [u8; 32]);
+
 type Bytes4 = [u8; 4];
-#[allow(dead_code)]
-type Bytes20 = [u8; 20];
+type Bytes20 = FixedVector<u8, typenum::U20>;
 type Bytes32 = [u8; 32];
 type Bytes48 = [u8; 48];
 type Bytes96 = FixedVector<u8, typenum::U96>;
@@ -23,18 +32,11 @@ type Version = Bytes4;
 type DomainType = Bytes4;
 #[allow(dead_code)]
 type ForkDigest = Bytes4;
-#[allow(dead_code)]
 type Domain = Bytes32;
 type BLSPubkey = Bytes48;
 type BLSSignature = Bytes96;
 #[allow(dead_code)]
 type ParticipationFlags = u8;
-#[allow(dead_code)]
 type Transaction = VariableList<u8, /* `MAX_BYTES_PER_TRANSACTION` */ typenum::U1073741824>;
-#[allow(dead_code)]
 type ExecutionAddress = Bytes20;
-#[allow(dead_code)]
 type WithdrawalIndex = u64;
-
-pub(crate) use beacon_chain::*;
-use ssz_types::{typenum, FixedVector, VariableList};
