6d1a3ea9cbc55767059db700a95e455e930619ff
Tomá <47506558+MegaRedHand@users.noreply.github.com>
2023-09-06 13:57:00 -0300

    Remove Elixir side duplicate structs

--- native/ssz_nif/src/ssz_types/beacon_chain.rs
+++ native/ssz_nif/src/ssz_types/beacon_chain.rs
@@ -1,237 +1,220 @@
-use super::config::{Config, Mainnet, Minimal};
+use super::config::Config;
 use super::*;
 use ssz_derive::{Decode, Encode};
 use ssz_types::{BitList, BitVector};
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct Fork {
-//     pub(crate) previous_version: Version,
-//     pub(crate) current_version: Version,
-//     pub(crate) epoch: Epoch,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct Fork {
+    pub(crate) previous_version: Version,
+    pub(crate) current_version: Version,
+    pub(crate) epoch: Epoch,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct ForkData {
-//     pub(crate) current_version: Version,
-//     pub(crate) genesis_validators_root: Root,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct ForkData {
+    pub(crate) current_version: Version,
+    pub(crate) genesis_validators_root: Root,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct Checkpoint {
-//     pub(crate) epoch: Epoch,
-//     pub(crate) root: Root,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct Checkpoint {
+    pub(crate) epoch: Epoch,
+    pub(crate) root: Root,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct Validator {
-//     pub(crate) pubkey: BLSPubkey,
-//     pub(crate) withdrawal_credentials: Bytes32,
-//     pub(crate) effective_balance: Gwei,
-//     pub(crate) slashed: bool,
-//     pub(crate) activation_eligibility_epoch: Epoch,
-//     pub(crate) activation_epoch: Epoch,
-//     pub(crate) exit_epoch: Epoch,
-//     pub(crate) withdrawable_epoch: Epoch,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct Validator {
+    pub(crate) pubkey: BLSPubkey,
+    pub(crate) withdrawal_credentials: Bytes32,
+    pub(crate) effective_balance: Gwei,
+    pub(crate) slashed: bool,
+    pub(crate) activation_eligibility_epoch: Epoch,
+    pub(crate) activation_epoch: Epoch,
+    pub(crate) exit_epoch: Epoch,
+    pub(crate) withdrawable_epoch: Epoch,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct AttestationData {
-//     pub(crate) slot: Slot,
-//     pub(crate) index: CommitteeIndex,
-//     pub(crate) beacon_block_root: Root,
-//     pub(crate) source: Checkpoint,
-//     pub(crate) target: Checkpoint,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct AttestationData {
+    pub(crate) slot: Slot,
+    pub(crate) index: CommitteeIndex,
+    pub(crate) beacon_block_root: Root,
+    pub(crate) source: Checkpoint,
+    pub(crate) target: Checkpoint,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct IndexedAttestationBase<C: Config> {
-//     pub(crate) attesting_indices: VariableList<ValidatorIndex, C::MaxValidatorsPerCommittee>,
-//     pub(crate) data: AttestationData,
-//     pub(crate) signature: BLSSignature,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct IndexedAttestation<C: Config> {
+    pub(crate) attesting_indices: VariableList<ValidatorIndex, C::MaxValidatorsPerCommittee>,
+    pub(crate) data: AttestationData,
+    pub(crate) signature: BLSSignature,
+}
 
-// pub(crate) type IndexedAttestation = IndexedAttestationBase<Mainnet>;
+#[derive(Encode, Decode)]
+pub(crate) struct PendingAttestation<C: Config> {
+    pub(crate) aggregation_bits: BitList<C::MaxValidatorsPerCommittee>,
+    pub(crate) data: AttestationData,
+    pub(crate) inclusion_delay: Slot,
+    pub(crate) proposer_index: ValidatorIndex,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct PendingAttestationBase<C: Config> {
-//     pub(crate) aggregation_bits: BitList<C::MaxValidatorsPerCommittee>,
-//     pub(crate) data: AttestationData,
-//     pub(crate) inclusion_delay: Slot,
-//     pub(crate) proposer_index: ValidatorIndex,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct Eth1Data {
+    pub(crate) deposit_root: Root,
+    pub(crate) deposit_count: u64,
+    pub(crate) block_hash: Hash32,
+}
 
-// pub(crate) type PendingAttestation = PendingAttestationBase<Mainnet>;
+#[derive(Encode, Decode)]
+pub(crate) struct HistoricalBatch<C: Config> {
+    pub(crate) block_roots: FixedVector<Root, C::SlotsPerHistoricalRoot>,
+    pub(crate) state_roots: FixedVector<Root, C::SlotsPerHistoricalRoot>,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct Eth1Data {
-//     pub(crate) deposit_root: Root,
-//     pub(crate) deposit_count: u64,
-//     pub(crate) block_hash: Hash32,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct DepositMessage {
+    pub(crate) pubkey: BLSPubkey,
+    pub(crate) withdrawal_credentials: Bytes32,
+    pub(crate) amount: Gwei,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct HistoricalBatchBase<C: Config> {
-//     pub(crate) block_roots: FixedVector<Root, C::SlotsPerHistoricalRoot>,
-//     pub(crate) state_roots: FixedVector<Root, C::SlotsPerHistoricalRoot>,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct DepositData {
+    pub(crate) pubkey: BLSPubkey,
+    pub(crate) withdrawal_credentials: Bytes32,
+    pub(crate) amount: Gwei,
+    pub(crate) signature: BLSSignature,
+}
 
-// pub(crate) type HistoricalBatch = HistoricalBatchBase<Mainnet>;
-// pub(crate) type HistoricalBatchMinimal = HistoricalBatchBase<Minimal>;
+#[derive(Encode, Decode)]
+pub(crate) struct HistoricalSummary {
+    pub(crate) block_summary_root: Root,
+    pub(crate) state_summary_root: Root,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct DepositMessage {
-//     pub(crate) pubkey: BLSPubkey,
-//     pub(crate) withdrawal_credentials: Bytes32,
-//     pub(crate) amount: Gwei,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct Deposit {
+    pub(crate) proof: FixedVector<Bytes32, /* DEPOSIT_CONTRACT_TREE_DEPTH + 1 */ typenum::U33>,
+    pub(crate) data: DepositData,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct Deposit {
-//     pub(crate) proof: FixedVector<Bytes32, /* DEPOSIT_CONTRACT_TREE_DEPTH + 1 */ typenum::U33>,
-//     pub(crate) data: DepositData,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct VoluntaryExit {
+    pub(crate) epoch: Epoch,
+    pub(crate) validator_index: ValidatorIndex,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct DepositData {
-//     pub(crate) pubkey: BLSPubkey,
-//     pub(crate) withdrawal_credentials: Bytes32,
-//     pub(crate) amount: Gwei,
-//     pub(crate) signature: BLSSignature,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct Attestation<C: Config> {
+    pub(crate) aggregation_bits: BitList<C::MaxValidatorsPerCommittee>,
+    pub(crate) data: AttestationData,
+    pub(crate) signature: BLSSignature,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct HistoricalSummary {
-//     pub(crate) block_summary_root: Root,
-//     pub(crate) state_summary_root: Root,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct BeaconBlockHeader {
+    pub(crate) slot: Slot,
+    pub(crate) proposer_index: ValidatorIndex,
+    pub(crate) parent_root: Root,
+    pub(crate) state_root: Root,
+    pub(crate) body_root: Root,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct VoluntaryExit {
-//     pub(crate) epoch: Epoch,
-//     pub(crate) validator_index: ValidatorIndex,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct SignedBeaconBlockHeader {
+    pub(crate) message: BeaconBlockHeader,
+    pub(crate) signature: BLSSignature,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct AttestationBase<C: Config> {
-//     pub(crate) aggregation_bits: BitList<C::MaxValidatorsPerCommittee>,
-//     pub(crate) data: AttestationData,
-//     pub(crate) signature: BLSSignature,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct AttesterSlashing<C: Config> {
+    pub(crate) attestation_1: IndexedAttestation<C>,
+    pub(crate) attestation_2: IndexedAttestation<C>,
+}
 
-// pub(crate) type Attestation = AttestationBase<Mainnet>;
+#[derive(Encode, Decode)]
+pub(crate) struct SigningData {
+    pub(crate) object_root: Root,
+    pub(crate) domain: Domain,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct BeaconBlockHeader {
-//     pub(crate) slot: Slot,
-//     pub(crate) proposer_index: ValidatorIndex,
-//     pub(crate) parent_root: Root,
-//     pub(crate) state_root: Root,
-//     pub(crate) body_root: Root,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct BLSToExecutionChange {
+    pub(crate) validator_index: ValidatorIndex,
+    pub(crate) from_bls_pubkey: BLSPubkey,
+    pub(crate) to_execution_address: ExecutionAddress,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct SignedBeaconBlockHeader {
-//     pub(crate) message: BeaconBlockHeader,
-//     pub(crate) signature: BLSSignature,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct SignedBLSToExecutionChange {
+    pub(crate) message: BLSToExecutionChange,
+    pub(crate) signature: BLSSignature,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct AttesterSlashingBase<C: Config> {
-//     pub(crate) attestation_1: IndexedAttestationBase<C>,
-//     pub(crate) attestation_2: IndexedAttestationBase<C>,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct SignedVoluntaryExit {
+    pub(crate) message: VoluntaryExit,
+    pub(crate) signature: BLSSignature,
+}
 
-// pub(crate) type AttesterSlashing = AttesterSlashingBase<Mainnet>;
+#[derive(Encode, Decode)]
+pub(crate) struct ProposerSlashing {
+    pub(crate) signed_header_1: SignedBeaconBlockHeader,
+    pub(crate) signed_header_2: SignedBeaconBlockHeader,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct SigningData {
-//     pub(crate) object_root: Root,
-//     pub(crate) domain: Domain,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct SyncAggregate<C: Config> {
+    pub(crate) sync_committee_bits: BitVector<C::SyncCommitteeSize>,
+    pub(crate) sync_committee_signature: BLSSignature,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct BLSToExecutionChange {
-//     pub(crate) validator_index: ValidatorIndex,
-//     pub(crate) from_bls_pubkey: BLSPubkey,
-//     pub(crate) to_execution_address: ExecutionAddress,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct Withdrawal {
+    pub(crate) index: WithdrawalIndex,
+    pub(crate) validator_index: ValidatorIndex,
+    pub(crate) address: ExecutionAddress,
+    pub(crate) amount: Gwei,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct SignedBLSToExecutionChange {
-//     pub(crate) message: BLSToExecutionChange,
-//     pub(crate) signature: BLSSignature,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct ExecutionPayloadHeader<C: Config> {
+    pub(crate) parent_hash: Hash32,
+    pub(crate) fee_recipient: ExecutionAddress,
+    pub(crate) state_root: Root,
+    pub(crate) receipts_root: Root,
+    pub(crate) logs_bloom: FixedVector<u8, C::BytesPerLogsBloom>,
+    pub(crate) prev_randao: Bytes32,
+    pub(crate) block_number: u64,
+    pub(crate) gas_limit: u64,
+    pub(crate) gas_used: u64,
+    pub(crate) timestamp: u64,
+    pub(crate) extra_data: VariableList<u8, C::MaxExtraDataBytes>,
+    pub(crate) base_fee_per_gas: Uint256,
+    pub(crate) block_hash: Hash32,
+    pub(crate) transactions_root: Root,
+    pub(crate) withdrawals_root: Root,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct SignedVoluntaryExit {
-//     pub(crate) message: VoluntaryExit,
-//     pub(crate) signature: BLSSignature,
-// }
+#[derive(Encode, Decode)]
+pub(crate) struct ExecutionPayload<C: Config> {
+    pub(crate) parent_hash: Hash32,
+    pub(crate) fee_recipient: ExecutionAddress,
+    pub(crate) state_root: Root,
+    pub(crate) receipts_root: Root,
+    pub(crate) logs_bloom: FixedVector<u8, C::BytesPerLogsBloom>,
+    pub(crate) prev_randao: Bytes32,
+    pub(crate) block_number: u64,
+    pub(crate) gas_limit: u64,
+    pub(crate) gas_used: u64,
+    pub(crate) timestamp: u64,
+    pub(crate) extra_data: VariableList<u8, C::MaxExtraDataBytes>,
+    pub(crate) base_fee_per_gas: Uint256,
+    pub(crate) block_hash: Hash32,
+    pub(crate) transactions: VariableList<Transaction, C::MaxTransactionsPerPayload>,
+    pub(crate) withdrawals: VariableList<Withdrawal, C::MaxWithdrawalsPerPayload>,
+}
 
-// #[derive(Encode, Decode)]
-// pub(crate) struct ProposerSlashing {
-//     pub(crate) signed_header_1: SignedBeaconBlockHeader,
-//     pub(crate) signed_header_2: SignedBeaconBlockHeader,
-// }
-
-// #[derive(Encode, Decode)]
-// pub(crate) struct SyncAggregateBase<C: Config> {
-//     pub(crate) sync_committee_bits: BitVector<C::SyncCommitteeSize>,
-//     pub(crate) sync_committee_signature: BLSSignature,
-// }
-
-// pub(crate) type SyncAggregate = SyncAggregateBase<Mainnet>;
-// pub(crate) type SyncAggregateMinimal = SyncAggregateBase<Minimal>;
-
-// #[derive(Encode, Decode)]
-// pub(crate) struct Withdrawal {
-//     pub(crate) index: WithdrawalIndex,
-//     pub(crate) validator_index: ValidatorIndex,
-//     pub(crate) address: ExecutionAddress,
-//     pub(crate) amount: Gwei,
-// }
-
-// #[derive(Encode, Decode)]
-// pub(crate) struct ExecutionPayloadHeaderBase<C: Config> {
-//     pub(crate) parent_hash: Hash32,
-//     pub(crate) fee_recipient: ExecutionAddress,
-//     pub(crate) state_root: Root,
-//     pub(crate) receipts_root: Root,
-//     pub(crate) logs_bloom: FixedVector<u8, C::BytesPerLogsBloom>,
-//     pub(crate) prev_randao: Bytes32,
-//     pub(crate) block_number: u64,
-//     pub(crate) gas_limit: u64,
-//     pub(crate) gas_used: u64,
-//     pub(crate) timestamp: u64,
-//     pub(crate) extra_data: VariableList<u8, C::MaxExtraDataBytes>,
-//     pub(crate) base_fee_per_gas: Uint256,
-//     pub(crate) block_hash: Hash32,
-//     pub(crate) transactions_root: Root,
-//     pub(crate) withdrawals_root: Root,
-// }
-
-// pub(crate) type ExecutionPayloadHeader = ExecutionPayloadHeaderBase<Mainnet>;
-
-// #[derive(Encode, Decode)]
-// pub(crate) struct ExecutionPayloadBase<C: Config> {
-//     pub(crate) parent_hash: Hash32,
-//     pub(crate) fee_recipient: ExecutionAddress,
-//     pub(crate) state_root: Root,
-//     pub(crate) receipts_root: Root,
-//     pub(crate) logs_bloom: FixedVector<u8, C::BytesPerLogsBloom>,
-//     pub(crate) prev_randao: Bytes32,
-//     pub(crate) block_number: u64,
-//     pub(crate) gas_limit: u64,
-//     pub(crate) gas_used: u64,
-//     pub(crate) timestamp: u64,
-//     pub(crate) extra_data: VariableList<u8, C::MaxExtraDataBytes>,
-//     pub(crate) base_fee_per_gas: Uint256,
-//     pub(crate) block_hash: Hash32,
-//     pub(crate) transactions: VariableList<Transaction, C::MaxTransactionsPerPayload>,
-//     pub(crate) withdrawals: VariableList<Withdrawal, C::MaxWithdrawalsPerPayload>,
-// }
-
-// pub(crate) type ExecutionPayload = ExecutionPayloadBase<Mainnet>;
 #[derive(Encode, Decode)]
 pub(crate) struct SyncCommittee<C: Config> {
     pub(crate) pubkeys: FixedVector<BLSPubkey, C::SyncCommitteeSize>,
--- native/ssz_nif/src/utils/mod.rs
+++ native/ssz_nif/src/utils/mod.rs
@@ -3,20 +3,10 @@ pub(crate) mod from_ssz;
 pub(crate) mod helpers;
 
 macro_rules! match_schema_and_encode {
-    (($schema:expr, $config:expr, $map:expr) => { $($t:tt),* $(,)? }) => {
+    (($schema:expr, $config:expr, $map:expr) => { $($t:ident $(<$_c:ident>)?),* $(,)? }) => {
         match $schema {
             $(
-                stringify!($t) => match $config {
-                    "MainnetConfig" => $crate::utils::helpers::encode_ssz::<
-                        elx_types::$t, ssz_types::$t<
-                            $crate::ssz_types::config::Mainnet
-                        >>($map),
-                    "MinimalConfig" => $crate::utils::helpers::encode_ssz::<
-                    elx_types::$t, ssz_types::$t<
-                        $crate::ssz_types::config::Minimal
-                    >>($map),
-                    _ => Err(rustler::Error::BadArg),
-                }
+                stringify!($t) => $crate::utils::encode_config_match!($config, $map, $t $(<$_c>)?),
             )*
             _ => Err(rustler::Error::BadArg),
         }
@@ -24,27 +14,65 @@ macro_rules! match_schema_and_encode {
 }
 
 macro_rules! match_schema_and_decode {
-    (($schema:expr, $config:expr, $bytes:expr, $env:expr) => { $($t:tt),* $(,)? }) => {
+    (($schema:expr, $config:expr, $bytes:expr, $env:expr) => { $($t:ident $(<$_c:ident>)?),* $(,)? }) => {
         match $schema {
             $(
-                stringify!($t) => match $config {
-                    "MainnetConfig" => $crate::utils::helpers::decode_ssz::<
-                        elx_types::$t, ssz_types::$t<
-                            $crate::ssz_types::config::Mainnet
-                        >>($bytes, $env),
-                    "MinimalConfig" => $crate::utils::helpers::decode_ssz::<
-                    elx_types::$t, ssz_types::$t<
-                        $crate::ssz_types::config::Minimal
-                    >>($bytes, $env),
-                    _ => Err(rustler::Error::BadArg),
-                }
+                stringify!($t) => $crate::utils::decode_config_match!($config, $bytes, $env, $t $(<$_c>)?),
             )*
             _ => Err(rustler::Error::BadArg),
         }
     };
 }
 
-macro_rules! gen_struct {
+macro_rules! encode_config_match {
+    ($config:expr, $map:expr, $t:ident<C>) => {
+        match $config {
+            "MainnetConfig" => $crate::utils::helpers::encode_ssz::<
+                elx_types::$t,
+                ssz_types::$t<$crate::ssz_types::config::Mainnet>,
+            >($map),
+            "MinimalConfig" => $crate::utils::helpers::encode_ssz::<
+                elx_types::$t,
+                ssz_types::$t<$crate::ssz_types::config::Minimal>,
+            >($map),
+            _ => Err(rustler::Error::BadArg),
+        }
+    };
+    ($config:expr, $map:expr, $t:ident) => {
+        match $config {
+            "MainnetConfig" | "MinimalConfig" => {
+                $crate::utils::helpers::encode_ssz::<elx_types::$t, ssz_types::$t>($map)
+            }
+            _ => Err(rustler::Error::BadArg),
+        }
+    };
+}
+
+macro_rules! decode_config_match {
+    ($config:expr, $bytes:expr, $env:expr, $t:ident<C>) => {
+        match $config {
+            "MainnetConfig" => $crate::utils::helpers::decode_ssz::<
+                elx_types::$t,
+                ssz_types::$t<$crate::ssz_types::config::Mainnet>,
+            >($bytes, $env),
+            "MinimalConfig" => $crate::utils::helpers::decode_ssz::<
+                elx_types::$t,
+                ssz_types::$t<$crate::ssz_types::config::Minimal>,
+            >($bytes, $env),
+            _ => Err(rustler::Error::BadArg),
+        }
+    };
+    ($config:expr, $bytes:expr, $env:expr, $t:ident) => {
+        match $config {
+            "MainnetConfig" | "MinimalConfig" => {
+                $crate::utils::helpers::decode_ssz::<elx_types::$t, ssz_types::$t>($bytes, $env)
+            }
+            _ => Err(rustler::Error::BadArg),
+        }
+    };
+}
+
+macro_rules! gen_struct_with_config {
     // Named-Struct
     (
         $( #[$meta:meta] )*
@@ -77,7 +105,7 @@ macro_rules! gen_struct {
             }
         }
 
-        impl< $($( $lt $( : $clt $(+ $dlt )* )? ),+)?, C: $crate::ssz_types::config::Config>
+        impl< $($( $lt $( : $clt $(+ $dlt )* )? ),+,)? C: $crate::ssz_types::config::Config>
         $crate::utils::from_elx::FromElx<$name$(< $( $lt $( : $clt $(+ $dlt )* )? ),+ >)?> for $crate::ssz_types::$name<C> {
             fn from(elx: $name) -> Result<Self, $crate::utils::from_elx::FromElxError> {
                 $(
@@ -91,6 +119,56 @@ macro_rules! gen_struct {
     }
 }
 
+macro_rules! gen_struct {
+    // Named-Struct
+    (
+        $( #[$meta:meta] )*
+    //  ^~~~attributes~~~~^
+        $vis:vis struct $name:ident$(< $( $lt:tt $( : $clt:tt $(+ $dlt:tt )* )? ),+ >)? {
+            $(
+                $( #[$field_meta:meta] )*
+    //          ^~~~field attributes~~~!^
+                $field_vis:vis $field_name:ident : $field_ty:ty
+    //          ^~~~~~~~~~~~~~~~~a single field~~~~~~~~~~~~~~~^
+            ),*
+        $(,)? }
+    ) => {
+        $( #[$meta] )*
+        #[derive(Clone)]
+        $vis struct $name$(< $( $lt $( : $clt $(+ $dlt )* )? ),+ >)? {
+            $(
+                $( #[$field_meta] )*
+                $field_vis $field_name : $field_ty
+            ),*
+        }
+        impl<'a> $crate::utils::from_ssz::FromSsz<'a, $crate::ssz_types::$name> for $name$(< $( $lt $( : $clt $(+ $dlt )* )? ),+ >)? {
+            fn from(ssz: $crate::ssz_types::$name, env: ::rustler::Env<'a>) -> Self {
+                $(
+                    let $field_name = $crate::utils::from_ssz::FromSsz::from(ssz.$field_name, env);
+                )*
+                Self {
+                    $($field_name),*
+                }
+            }
+        }
+
+        impl< $($( $lt $( : $clt $(+ $dlt )* )? ),+)?>
+        $crate::utils::from_elx::FromElx<$name$(< $( $lt $( : $clt $(+ $dlt )* )? ),+ >)?> for $crate::ssz_types::$name {
+            fn from(elx: $name) -> Result<Self, $crate::utils::from_elx::FromElxError> {
+                $(
+                    let $field_name = $crate::utils::from_elx::FromElx::from(elx.$field_name)?;
+                )*
+                Ok(Self {
+                    $($field_name),*
+                })
+            }
+        }
+    }
+}
+
+pub(crate) use decode_config_match;
+pub(crate) use encode_config_match;
 pub(crate) use gen_struct;
+pub(crate) use gen_struct_with_config;
 pub(crate) use match_schema_and_decode;
 pub(crate) use match_schema_and_encode;
