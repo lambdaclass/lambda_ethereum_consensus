64fc978d0c92722c3cbc596b2506f000ca97d805
Tomás Arjovsky <t.arjovsky@gmail.com>
2023-09-12 14:39:20 +0200

    address credo comments

--- .credo.exs
+++ .credo.exs
@@ -23,13 +23,7 @@
         #
         included: [
           "lib/",
-          "src/",
-          "test/",
-          "web/",
-          "apps/*/lib/",
-          "apps/*/src/",
-          "apps/*/test/",
-          "apps/*/web/"
+          "test/"
         ],
         excluded: [~r"/_build/", ~r"/deps/", ~r"/node_modules/"]
       },
@@ -87,12 +81,6 @@
           # {Credo.Check.Design.AliasUsage,
           # [priority: :low, if_nested_deeper_than: 2, if_called_more_often_than: 0]},
 
-          # You can also customize the exit_status of each check.
-          # If you don't want TODO comments to cause `mix credo` to fail, just
-          # set this value to 0 (zero).
-          #
-          {Credo.Check.Design.TagTODO, [exit_status: 2]},
-          {Credo.Check.Design.TagFIXME, []},
 
           #
           ## Readability Checks
@@ -184,6 +172,8 @@
           {Credo.Check.Readability.OnePipePerLine, []},
           {Credo.Check.Refactor.MapMap, []},
           {Credo.Check.Refactor.ModuleDependencies, []},
+          {Credo.Check.Design.TagTODO, []},
+          {Credo.Check.Design.TagFIXME, []},
         ]
       }
     }
