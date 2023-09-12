7ad39bb65fe525259cca915f2d7210de992318f9
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2023-09-12 14:29:26 +0200

    fixes

--- lib/beacon_api/controllers/v1/beacon_controller.ex
+++ lib/beacon_api/controllers/v1/beacon_controller.ex
@@ -8,36 +8,27 @@ defmodule BeaconApi.V1.BeaconController do
     state_id =
       Map.get(params, "state_id")
 
-    case state_id in [
-           "head",
-           "genesis",
-           "finalized",
-           "justified",
-           "0xbe41d3394dc8c461ab7049dbedb8944613ada8bd1743e091948f4d7b5ca8af36"
-         ] do
-      true ->
-        conn
-        |> json(%{
-          execution_optimistic: true,
-          finalized: false,
-          data: %{
-            root: "0xbe41d3394dc8c461ab7049dbedb8944613ada8bd1743e091948f4d7b5ca8af36"
-          }
-        })
-
-      false ->
-        if is_integer(
-             try do
-               String.to_integer(state_id)
-             rescue
-               _ -> nil
-             end
-           ) or
-             Utils.is_bytes32?(state_id) do
-          conn |> ErrorController.not_found(nil)
-        else
-          conn |> ErrorController.bad_request("Invalid state ID: #{state_id}")
-        end
+    if state_id in [
+         "head",
+         "genesis",
+         "finalized",
+         "justified",
+         "0xbe41d3394dc8c461ab7049dbedb8944613ada8bd1743e091948f4d7b5ca8af36"
+       ] do
+      conn
+      |> json(%{
+        execution_optimistic: true,
+        finalized: false,
+        data: %{
+          root: "0xbe41d3394dc8c461ab7049dbedb8944613ada8bd1743e091948f4d7b5ca8af36"
+        }
+      })
+    else
+      if match?({_, ""}, Integer.parse(state_id)) or Utils.is_bytes32?(state_id) do
+        conn |> ErrorController.not_found(nil)
+      else
+        conn |> ErrorController.bad_request("Invalid state ID: #{state_id}")
+      end
     end
   end
 end
--- lib/beacon_api/error_json.ex
+++ lib/beacon_api/error_json.ex
@@ -1,7 +1,7 @@
 defmodule BeaconApi.ErrorJSON do
   use BeaconApi, :controller
 
-  @spec render(any, any) :: %{message: <<_::184>>}
+  @spec render(any, any) :: %{message: String.t()}
   def render(_, _) do
     %{
       message: "There has been an error"
