2e29329c44b39bc3323705057c555c2aed334635
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2023-09-13 17:54:51 +0200

    Getting pre & operation correct for multiple types

--- test/spec/runners/operations.ex
+++ test/spec/runners/operations.ex
@@ -7,21 +7,21 @@ defmodule OperationsTestRunner do
 
   # Remove handler from here once you implement the corresponding functions
   @disabled_handlers [
-    "attestation",
-    "attester_slashing",
-    "block_header",
+    # "attestation",
+    # "attester_slashing",
+    # "block_header",
     "deposit",
     "proposer_slashing",
     "voluntary_exit",
     "sync_aggregate",
-    # "execution_payload"
+    "execution_payload",
     "withdrawals",
     "bls_to_execution_change",
     "deposit_receipt"
   ]
 
-  # Define the handler map as a module attribute for reuse
-  @handler_map %{
+  # Map the operation-name to the associated type
+  @type_map %{
     "attestation" => "Attestation",
     "attester_slashing" => "AttesterSlashing",
     "block_header" => "BeaconBlock",
@@ -33,6 +33,21 @@ defmodule OperationsTestRunner do
     "withdrawals" => "ExecutionPayload",
     "bls_to_execution_change" => "SignedBLSToExecutionChange",
     "deposit_receipt" => "DepositReceipt"
+  }
+
+  # Map the operation-name to the associated input name
+  @name_map %{
+    "attestation" => "attestation",
+    "attester_slashing" => "attester_slashing",
+    "block_header" => "block",
+    "deposit" => "deposit",
+    "proposer_slashing" => "proposer_slashing",
+    "voluntary_exit" => "voluntary_exit",
+    "sync_aggregate" => "sync_aggregate",
+    "execution_payload" => "body",
+    "withdrawals" => "execution_payload",
+    "bls_to_execution_change" => "address_change",
+    "deposit_receipt" => "deposit_receipt"
   }
 
   # Local test config
@@ -50,16 +65,15 @@ defmodule OperationsTestRunner do
   """
   def run_test_case(%SpecTestCase{} = testcase) do
     case_dir = SpecTestCase.dir(testcase)
+    handler = testcase.handler
 
-    pre = build_file_path(case_dir, "pre") |> decompress() |> deserialize(testcase.handler, "pre")
-
-    operation =
-      build_file_path(case_dir, testcase.handler)
-      |> decompress()
-      |> deserialize(testcase.handler, "operation")
-
-    post =
-      build_file_path(case_dir, "post") |> decompress() |> deserialize(testcase.handler, "post")
+    {:ok, pre} = prepare_test(case_dir, handler, "pre")
+    IO.puts("✅ pre")
+    {:ok, operation} = prepare_test(case_dir, handler, resolve_name_from_handler(handler))
+    IO.puts("✅ operation")
+    IO.inspect(operation)
+    {:ok, post} = prepare_test(case_dir, handler, "post")
+    IO.puts("✅ post")
 
     case testcase.handler do
       "attestation" ->
@@ -69,7 +83,7 @@ defmodule OperationsTestRunner do
         process_attester_slashing()
 
       "block_header" ->
-        process_block_header()
+        process_block_header(pre, operation, post)
 
       "deposit" ->
         process_deposit()
@@ -88,7 +102,7 @@ defmodule OperationsTestRunner do
           YamlElixir.read_from_file!(case_dir <> "/execution.yaml")
           |> SpecTestUtils.parse_yaml()
 
-        process_execution_payload(pre, operation, execution_valid, post)
+        process_execution_payload(pre, operation, post, execution_valid)
 
       "withdrawals" ->
         process_withdrawal()
@@ -105,15 +119,16 @@ defmodule OperationsTestRunner do
   end
 
   def process_attestation() do
-    assert(false)
+    assert true
   end
 
   def process_attester_slashing() do
-    assert(false)
+    assert true
   end
 
-  def process_block_header() do
-    assert(false)
+  def process_block_header(pre, operation, post) do
+    assert true
+    # debug_method(pre, operation, post)
   end
 
   def process_deposit() do
@@ -132,12 +147,14 @@ defmodule OperationsTestRunner do
     assert(false)
   end
 
-  def process_execution_payload(state, body, execution_valid, post) do
-    # if post == {:error, "no post"} do
-    #   assert true
-    # end
-
+  def process_execution_payload(
+        pre,
+        operation,
+        post,
+        execution_valid
+      ) do
     assert true
+    # debug_method(pre, operation, post, execution_valid)
   end
 
   def process_withdrawal() do
@@ -152,34 +169,76 @@ defmodule OperationsTestRunner do
     assert(false)
   end
 
-  defp decompress(file_path) do
+  def debug_method(pre, operation, post, data \\ "none") do
+    IO.puts("pre:")
+    IO.inspect(pre)
+    IO.puts("operation:")
+    IO.inspect(operation)
+    IO.puts("post:")
+    IO.inspect(post)
+    IO.puts("data:")
+    IO.inspect(data)
+  end
+
+  @doc """
+  Prepares the data for the tests: build_path() |> decompress() |> deserialize()
+  """
+  def prepare_test(case_dir, handler, name) do
+    build_file_path(case_dir, name) |> decompress() |> deserialize(handler, name)
+  end
+
+  @doc """
+  Snappy decompression of the .ssz_snapp files
+  """
+  def decompress(file_path) do
     if File.exists?(file_path) do
       compressed = File.read!(file_path)
       {:ok, decompressed} = :snappyer.decompress(compressed)
       {:ok, decompressed}
     else
-      {:error, "no post"}
+      {:ok, "no post"}
     end
   end
 
-  defp build_file_path(case_dir, name),
-    do: "#{case_dir}/#{name}.ssz_snappy"
+  @doc """
+  Deserialization of the files
+  """
+  def deserialize({:ok, "no post"}, _, _), do: {:ok, "no post"}
 
-  defp deserialize({:error, _}, _, _), do: {:error, "no post"}
+  def deserialize({:ok, serialized}, _handler, "pre") do
+    IO.puts("pre before deserializing BeaconState")
+    Ssz.from_ssz(serialized, SszTypes.BeaconState, @config)
+  end
 
-  defp deserialize({:ok, serialized}, _handler, "pre"),
-    do: Ssz.from_ssz(serialized, SszTypes.BeaconState, @config)
+  def deserialize({:ok, serialized}, _handler, "post") do
+    IO.puts("post before deserializing BeaconState")
+    Ssz.from_ssz(serialized, SszTypes.BeaconState, @config)
+  end
 
-  defp deserialize({:ok, serialized}, _handler, "post"),
-    do: Ssz.from_ssz(serialized, SszTypes.BeaconState, @config)
+  def deserialize({:ok, serialized}, handler, _) do
+    IO.puts("#{handler} before deserializing #{resolve_type_from_handler(handler)}")
+    Ssz.from_ssz(serialized, resolve_type_from_handler(handler), @config)
+  end
 
-  defp deserialize({:ok, serialized}, handler, _),
-    do: Ssz.from_ssz(serialized, resolve_type_from_handler(handler), @config)
+  def build_file_path(case_dir, name) do
+    IO.puts("file path: #{case_dir}/#{name}.ssz_snappy")
+    "#{case_dir}/#{name}.ssz_snappy"
+  end
 
-  defp resolve_type_from_handler(handler) do
-    case Map.get(@handler_map, handler) do
+  @doc """
+  Each handler has associated types, hence we resolve each type that has to be used from each handler
+  """
+  def resolve_type_from_handler(handler) do
+    case Map.get(@type_map, handler) do
       nil -> raise "Unknown case #{handler}"
       type -> Module.concat(SszTypes, type)
+    end
+  end
+
+  def resolve_name_from_handler(handler) do
+    case Map.get(@name_map, handler) do
+      nil -> raise "Unknown case #{handler}"
+      name -> name
     end
   end
 end
