f5972c625d32c0e910cf0b75dfd29e7effb0dba5
Tomá <47506558+MegaRedHand@users.noreply.github.com>
2023-09-14 18:34:19 -0300

    Add typespecs

--- lib/lambda_ethereum_consensus/store/beacon_state.ex
+++ lib/lambda_ethereum_consensus/store/beacon_state.ex
@@ -9,10 +9,12 @@ defmodule LambdaEthereumConsensus.Store.BeaconState do
     Agent.start_link(fn -> nil end, name: __MODULE__)
   end
 
+  @spec get_state() :: BeaconState.t() | nil
   def get_state do
     Agent.get(__MODULE__, & &1)
   end
 
+  @spec set_state(BeaconState.t()) :: :ok
   def set_state(%BeaconState{} = new_state) do
     Agent.update(__MODULE__, fn _ -> new_state end)
   end
