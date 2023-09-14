95718c0345b248b5078b682c902ca8671419c605
Tomá <47506558+MegaRedHand@users.noreply.github.com>
2023-09-14 16:17:06 -0300

    Fix errors

--- lib/lambda_ethereum_consensus/beacon/helper_functions.ex
+++ lib/lambda_ethereum_consensus/beacon/helper_functions.ex
@@ -1,14 +1,22 @@
 defmodule LambdaEthereumConsensus.Beacon.HelperFunctions do
+  @moduledoc """
+  Implementation of the helper functions defined in the beacon-chain spec document.
+  """
+
   @doc """
   Return the 32-byte fork data root for the ``current_version`` and ``genesis_validators_root``.
   This is used primarily in signature domains to avoid collisions across forks/chains.
   """
   @spec compute_fork_data_root(SszTypes.version(), SszTypes.root()) :: SszTypes.root()
-  def compute_fork_data_root(current_version, genesis_validators_root) do
-    Ssz.hash_tree_root(%SszTypes.ForkData{
-      current_version: current_version,
-      genesis_validators_root: genesis_validators_root
-    })
+  def compute_fork_data_root(<<current_version::4>>, <<genesis_validators_root::32>>) do
+    # Should never fail
+    {:ok, root} =
+      Ssz.hash_tree_root(%SszTypes.ForkData{
+        current_version: current_version,
+        genesis_validators_root: genesis_validators_root
+      })
+
+    root
   end
 
   @doc """
@@ -17,7 +25,7 @@ defmodule LambdaEthereumConsensus.Beacon.HelperFunctions do
   4-bytes suffices for practical separation of forks/chains.
   """
   @spec compute_fork_digest(SszTypes.version(), SszTypes.root()) :: SszTypes.fork_digest()
-  def compute_fork_digest(current_version, genesis_validators_root) do
+  def compute_fork_digest(<<current_version::4>>, <<genesis_validators_root::32>>) do
     compute_fork_data_root(current_version, genesis_validators_root)
     |> binary_part(0, 4)
   end
