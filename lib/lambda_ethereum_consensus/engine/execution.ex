84fca213d7aeb182c07f75cc08fe3bae0692e021
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2023-09-14 17:58:44 +0200

    Added back execution file

---
+++ lib/lambda_ethereum_consensus/engine/execution.ex
@@ -0,0 +1,33 @@
+defmodule LambdaEthereumConsensus.Engine.Execution do
+  @moduledoc """
+  Execution Layer Engine API methods
+  """
+  alias LambdaEthereumConsensus.RPC
+
+  # Default Execution Layer endpoint
+  @execution_engine_endpoint "http://0.0.0.0:8551"
+
+  # Default Execution Layer RPC version
+  @execution_engine_rpc_version "2.0"
+
+  @doc """
+  Using this method Execution and consensus layer client software may
+  exchange with a list of supported Engine API methods.
+  """
+  @spec engine_exchange_capabilities(list) :: {:ok, any} | {:error, any}
+  def engine_exchange_capabilities(capabilities) do
+    params = %{
+      "capabilities" => capabilities
+    }
+
+    with {:ok, result} <-
+           RPC.call(
+             "engine_exchangeCapabilities",
+             @execution_engine_endpoint,
+             @execution_engine_rpc_version,
+             params
+           ) do
+      RPC.validate_response(result)
+    end
+  end
+end
