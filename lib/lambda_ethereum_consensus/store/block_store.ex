fc5c7daff2794832e770fe1980d77fe0e0f0581b
Martin Paulucci <martin.paulucci@lambdaclass.com>
2023-09-14 16:14:16 +0200

    Use hash tree root as block id.

--- lib/lambda_ethereum_consensus/store/block_store.ex
+++ lib/lambda_ethereum_consensus/store/block_store.ex
@@ -7,14 +7,16 @@ defmodule LambdaEthereumConsensus.Store.BlockStore do
 
   @spec store_block(SszTypes.BeaconBlock.t()) :: any
   def store_block(%SszTypes.BeaconBlock{} = block) do
-    key = @prefix <> block.state_root
+    {:ok, block_root} = Ssz.hash_tree_root(block)
     {:ok, encoded_block} = Ssz.to_ssz(block)
+
+    key = @prefix <> block_root
     LambdaEthereumConsensus.Store.Db.put(key, encoded_block)
   end
 
-  @spec get_block(binary()) :: {:ok, struct()} | {:error, String.t()} | :not_found
-  def get_block(state_root) do
-    key = @prefix <> state_root
+  @spec get_block(SszTypes.root()) :: {:ok, struct()} | {:error, String.t()} | :not_found
+  def get_block(block_root) do
+    key = @prefix <> block_root
 
     case LambdaEthereumConsensus.Store.Db.get(key) do
       :not_found ->
