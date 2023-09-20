fbfdee0529cd4d5150bcd9a8370ee17d07b2b6f8
Tomá <47506558+MegaRedHand@users.noreply.github.com>
2023-09-20 10:42:52 -0300

    Add wrappers around `get_key` for each "table"

--- lib/lambda_ethereum_consensus/store/block_store.ex
+++ lib/lambda_ethereum_consensus/store/block_store.ex
@@ -11,15 +11,15 @@ defmodule LambdaEthereumConsensus.Store.BlockStore do
     {:ok, block_root} = Ssz.hash_tree_root(block)
     {:ok, encoded_block} = Ssz.to_ssz(block)
 
-    key = get_key(@block_prefix, block_root)
+    key = block_key(block_root)
     LambdaEthereumConsensus.Store.Db.put(key, encoded_block)
-    slothash_key = get_key(@slothash_prefix, block.slot)
+    slothash_key = root_by_slot_key(block.slot)
     LambdaEthereumConsensus.Store.Db.put(slothash_key, block_root)
   end
 
   @spec get_block(SszTypes.root()) :: {:ok, struct()} | {:error, String.t()} | :not_found
   def get_block(block_root) do
-    key = get_key(@block_prefix, block_root)
+    key = block_key(block_root)
 
     with {:ok, block} <- LambdaEthereumConsensus.Store.Db.get(key) do
       Ssz.from_ssz(block, SszTypes.BeaconBlock)
@@ -29,7 +29,7 @@ defmodule LambdaEthereumConsensus.Store.BlockStore do
   @spec get_block_root_by_slot(SszTypes.slot()) ::
           {:ok, SszTypes.root()} | {:error, String.t()} | :not_found
   def get_block_root_by_slot(slot) do
-    key = get_key(@slothash_prefix, slot)
+    key = root_by_slot_key(slot)
     LambdaEthereumConsensus.Store.Db.get(key)
   end
 
@@ -40,6 +40,9 @@ defmodule LambdaEthereumConsensus.Store.BlockStore do
       get_block(root)
     end
   end
+
+  defp block_key(root), do: get_key(@block_prefix, root)
+  defp root_by_slot_key(slot), do: get_key(@slothash_prefix, slot)
 
   defp get_key(prefix, suffix) when is_integer(suffix) do
     prefix <> :binary.encode_unsigned(suffix)
