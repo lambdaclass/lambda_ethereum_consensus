996d58219ddfa986bbcab847d5430804bf53fe9e
Tomá <47506558+MegaRedHand@users.noreply.github.com>
2023-09-21 12:32:38 -0300

    Improve docs

--- test/spec/utils/runner_behaviour.ex
+++ test/spec/utils/runner_behaviour.ex
@@ -4,8 +4,7 @@ defmodule TestRunner do
   """
 
   @doc """
-  Adds behaviour and default implementation for `skip?/1` to the
-  user module.
+  Adds behaviour and default implementation for `skip?/1` (run all tests).
   """
   defmacro __using__(_) do
     quote([]) do
@@ -21,7 +20,8 @@ defmodule TestRunner do
   @callback skip?(testcase :: SpecTestCase.t()) :: boolean
 
   @doc """
-  Runs the given test case.
+  Runs the given test case. This function should only return
+  if the test case passes. To ignore test cases use `skip?/1`.
   """
   @callback run_test_case(testcase :: SpecTestCase.t()) :: any
 end
