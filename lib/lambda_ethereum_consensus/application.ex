d888fea1658854b54291e5a3998febfd1434fce0
Paul-Henry Kajfasz <42912740+phklive@users.noreply.github.com>
2023-09-27 17:44:36 +0200

    feat: added get requests boilerplate + added sync_from_checkpoint fn (#161)
    Co-authored-by: Tomás <47506558+MegaRedHand@users.noreply.github.com>

--- lib/lambda_ethereum_consensus/application.ex
+++ lib/lambda_ethereum_consensus/application.ex
@@ -2,11 +2,25 @@ defmodule LambdaEthereumConsensus.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
+  alias LambdaEthereumConsensus.Utils
 
   use Application
 
   @impl true
   def start(_type, _args) do
+    # Parse command line arguments
+    {args, _remaining_args, _errors} =
+      OptionParser.parse(System.argv(), switches: [checkpoint_sync: :string])
+
+    # Check for the --checkpoint-sync argument and act accordingly
+    case Keyword.get(args, :checkpoint_sync) do
+      nil ->
+        :ok
+
+      value ->
+        Utils.sync_from_checkpoint(value)
+    end
+
     {:ok, host} = Libp2p.host_new()
     {:ok, gsub} = Libp2p.new_gossip_sub(host)
 
