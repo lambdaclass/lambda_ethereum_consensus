279a1a08ec4087a84348f57f1f672be0722d0582
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2023-09-27 15:47:39 +0200

    Added newPayloadV2 function

--- lib/lambda_ethereum_consensus/engine/execution.ex
+++ lib/lambda_ethereum_consensus/engine/execution.ex
@@ -11,6 +11,33 @@ defmodule LambdaEthereumConsensus.Engine.Execution do
   @execution_engine_rpc_version "2.0"
 
   @doc """
+  Notifies the Execution client of a new payload
+  """
+  @spec notify_new_payload(SszTypes.ExecutionPayload.t()) :: {:ok, any} | {:error, any}
+  def notify_new_payload(execution_payload) do
+    params = %{"ExecutionPayload" => execution_payload}
+
+    case RPC.call(
+             "engine_newPayloadV2",
+             @execution_engine_endpoint,
+             @execution_engine_rpc_version,
+             params
+           ) do
+      {:ok, result} -> {:ok, result}
+      error -> {:error, error}
+    end
+  end
+
+  @doc """
+  Verifies the validity of the data contained in the new payload and notifies the Execution client of a new payload
+  """
+  @spec verify_and_notify_new_payload(SszTypes.ExecutionPayload.t()) :: {:ok, any} | {:error, any}
+  def verify_and_notify_new_payload(execution_payload) do
+    # Add verification layer
+    notify_new_payload(execution_payload)
+  end
+
+  @doc """
   Using this method Execution and consensus layer client software may
   exchange with a list of supported Engine API methods.
   """
