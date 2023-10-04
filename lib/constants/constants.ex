b444b0da262ef3da0017e7407536204d20abdd5e
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2023-10-04 17:08:35 +0200

    Add moduledoc

--- lib/constants/constants.ex
+++ lib/constants/constants.ex
@@ -1,4 +1,8 @@
 defmodule Constants do
+  @moduledoc """
+  Single entrypoint for fetching eth-spec constants.
+  """
+
   # NOTE: this only works for Capella
   def get(name) do
     config = Application.get_env(__MODULE__, :config, MainnetConfig)
