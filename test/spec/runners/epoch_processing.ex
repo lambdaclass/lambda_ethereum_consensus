d4c6c4c4eb39ab167f062190984857f66eae0c1b
Godspower Eze <61994334+Godspower-Eze@users.noreply.github.com>
2023-10-11 13:47:10 +0100

    feat: added process_randao_mixes_reset (#290)
    Co-authored-by: Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>

--- test/spec/runners/epoch_processing.ex
+++ test/spec/runners/epoch_processing.ex
@@ -18,7 +18,7 @@ defmodule EpochProcessingTestRunner do
     # "eth1_data_reset",
     "effective_balance_updates",
     "slashings_reset",
-    "randao_mixes_reset",
+    # "randao_mixes_reset",
     "historical_summaries_update",
     "participation_record_updates",
     "participation_flag_updates",
@@ -55,6 +55,11 @@ defmodule EpochProcessingTestRunner do
 
   def handle_case("eth1_data_reset", pre_state, post_state) do
     result = EpochProcessing.process_eth1_data_reset(pre_state)
+    assert {:ok, post_state} == result
+  end
+
+  def handle_case("randao_mixes_reset", pre_state, post_state) do
+    result = EpochProcessing.process_randao_mixes_reset(pre_state)
     assert {:ok, post_state} == result
   end
 end
