0a5c59209daaeea62416dd8772506a7f0e07500a
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2023-10-12 17:02:21 -0300

    Add code for receiving subscription messages

--- native/libp2p_port/internal/subscriptions/subscriptions.go
+++ native/libp2p_port/internal/subscriptions/subscriptions.go
@@ -2,7 +2,6 @@ package gossipsub
 
 import (
 	"context"
-	"fmt"
 	"math"
 	"time"
 
@@ -17,6 +16,7 @@ import (
 
 type Subscriber struct {
 	subscriptions map[string]chan struct{}
+	topics        map[string]*pubsub.Topic
 	gsub          *pubsub.PubSub
 	port          *port.Port
 }
@@ -73,34 +73,45 @@ func NewSubscriber(p *port.Port, h host.Host) Subscriber {
 	return Subscriber{subscriptions: make(map[string]chan struct{}), gsub: gsub, port: p}
 }
 
-func (s *Subscriber) Subscribe(topic_name string) {
-	_, is_subscribed := s.subscriptions[topic_name]
-	if !is_subscribed {
-		s.subscriptions[topic_name] = make(chan struct{}, 1)
-		go SubscribeToTopic(topic_name, s.subscriptions[topic_name], s.port)
+func (s *Subscriber) Subscribe(topicName string) {
+	_, isSubscribed := s.subscriptions[topicName]
+	if isSubscribed {
+		return
 	}
+	topic, err := s.gsub.Join(topicName)
+	utils.PanicIfError(err)
+	s.topics[topicName] = topic
+	sub, err := topic.Subscribe()
+	utils.PanicIfError(err)
+	s.subscriptions[topicName] = make(chan struct{}, 1)
+	go SubscribeToTopic(sub, s.subscriptions[topicName], s.port)
 }
 
-func (s *Subscriber) Unsubscribe(topic_name string) {
-	_, is_subscribed := s.subscriptions[topic_name]
-	if is_subscribed {
-		s.subscriptions[topic_name] <- struct{}{}
-		delete(s.subscriptions, topic_name)
+func (s *Subscriber) Unsubscribe(topicName string) {
+	_, isSubscribed := s.subscriptions[topicName]
+	if !isSubscribed {
+		return
 	}
+	s.subscriptions[topicName] <- struct{}{}
+	delete(s.subscriptions, topicName)
+	s.topics[topicName].Close()
+	delete(s.topics, topicName)
 }
 
-func SubscribeToTopic(name string, stop chan struct{}, p *port.Port) {
-	// This is mocked for the PoC. An actual subscription should call libp2p.
-	i := 0
+func SubscribeToTopic(sub *pubsub.Subscription, stop chan struct{}, p *port.Port) {
+	topic := sub.Topic()
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		<-stop
+		cancel()
+	}()
 	for {
-		select {
-		case <-stop:
+		msg, err := sub.Next(ctx)
+		if err == context.Canceled {
 			return
-		default:
-			notification := proto_helpers.GossipNotification(name, []byte(fmt.Sprintf("Mock notification %d", i)))
-			p.SendNotification(&notification)
-			i += 1
-			time.Sleep(1 * time.Second)
 		}
+		utils.PanicIfError(err)
+		notification := proto_helpers.GossipNotification(topic, msg.Data)
+		p.SendNotification(&notification)
 	}
 }
