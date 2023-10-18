925d433133ae29caaf99970a78a6fad12d4c766c
Martin Paulucci <martin.paulucci@lambdaclass.com>
2023-10-18 18:36:40 +0200

    Add `get_forkchoice_store` function.

--- lib/lambda_ethereum_consensus/fork_choice/store.ex
+++ lib/lambda_ethereum_consensus/fork_choice/store.ex
@@ -6,46 +6,11 @@ defmodule LambdaEthereumConsensus.ForkChoice.Store do
   use GenServer
   require Logger
 
-  alias LambdaEthereumConsensus.P2P.BlockDownloader
+  alias LambdaEthereumConsensus.ForkChoice.Utils
   alias LambdaEthereumConsensus.Store.BlockStore
+  alias SszTypes.BeaconBlock
   alias SszTypes.BeaconState
-
-  defmodule Store do
-    @moduledoc """
-      The Store struct is used to track information required for the fork choice algorithm.
-    """
-    defstruct [
-      :time,
-      :genesis_time,
-      :justified_checkpoint,
-      :finalized_checkpoint,
-      :unrealized_justified_checkpoint,
-      :unrealized_finalized_checkpoint,
-      :proposer_boost_root,
-      :equivocating_indices,
-      :blocks,
-      :block_states,
-      :checkpoint_states,
-      :latest_messages,
-      :unrealized_justifications
-    ]
-
-    @type t :: %Store{
-            time: SszTypes.uint64(),
-            genesis_time: SszTypes.uint64(),
-            justified_checkpoint: SszTypes.Checkpoint.t() | nil,
-            finalized_checkpoint: SszTypes.Checkpoint.t(),
-            unrealized_justified_checkpoint: SszTypes.Checkpoint.t() | nil,
-            unrealized_finalized_checkpoint: SszTypes.Checkpoint.t() | nil,
-            proposer_boost_root: SszTypes.root() | nil,
-            equivocating_indices: MapSet.t(SszTypes.validator_index()),
-            blocks: %{SszTypes.root() => SszTypes.BeaconBlock.t()},
-            block_states: %{SszTypes.root() => SszTypes.BeaconState.t()},
-            checkpoint_states: %{SszTypes.Checkpoint.t() => SszTypes.BeaconState.t()},
-            latest_messages: %{SszTypes.validator_index() => SszTypes.Checkpoint.t()},
-            unrealized_justifications: %{SszTypes.root() => SszTypes.Checkpoint.t()}
-          }
-  end
+  alias SszTypes.Store
 
   ##########################
   ### Public API
@@ -85,27 +50,16 @@ defmodule LambdaEthereumConsensus.ForkChoice.Store do
   ##########################
 
   @impl GenServer
-  @spec init({BeaconState.t(), Libp2p.host()}) :: {:ok, Store.t()}
-  def init({initial_state = %SszTypes.BeaconState{}, host}) do
-    store = %Store{
-      time: DateTime.to_unix(DateTime.utc_now()),
-      genesis_time: initial_state.genesis_time,
-      justified_checkpoint: initial_state.current_justified_checkpoint,
-      finalized_checkpoint: initial_state.finalized_checkpoint,
-      unrealized_justified_checkpoint: nil,
-      unrealized_finalized_checkpoint: nil,
-      proposer_boost_root: nil,
-      equivocating_indices: MapSet.new(),
-      blocks: %{},
-      block_states: %{},
-      checkpoint_states: %{},
-      latest_messages: %{},
-      unrealized_justifications: %{}
-    }
+  @spec init({BeaconState.t(), BeaconBlock.t()}) :: {:ok, Store.t()} | {:stop, any}
+  def init({anchor_state = %BeaconState{}, anchor_block = %BeaconBlock{}}) do
+    case Utils.get_forkchoice_store(anchor_state, anchor_block) do
+      {:ok, store = %Store{}} ->
+        Logger.info("[Fork choice] Initialized store.")
+        {:ok, store}
 
-    Process.send_after(self(), {:fetch_initial_block, initial_state, host}, 0)
-
-    {:ok, store}
+      {:error, error} ->
+        {:stop, error}
+    end
   end
 
   @impl GenServer
@@ -118,31 +72,6 @@ defmodule LambdaEthereumConsensus.ForkChoice.Store do
     Logger.info("[Fork choice] Adding block #{block_root} to the store.")
     :ok = BlockStore.store_block(block)
     {:noreply, Map.put(state, :blocks, Map.put(state.blocks, block_root, block))}
-  end
-
-  @impl GenServer
-  def handle_info({:fetch_initial_block, initial_state, host}, state) do
-    {:ok, state_root} = Ssz.hash_tree_root(initial_state)
-
-    # The latest_block_header.state_root was zeroed out to avoid circular dependencies
-    {:ok, block_root} =
-      Ssz.hash_tree_root(Map.put(initial_state.latest_block_header, :state_root, state_root))
-
-    case BlockDownloader.request_block_by_root(block_root, host) do
-      {:ok, signed_block} ->
-        Logger.info("[Checkpoint sync] Initial block fetched.")
-        block = signed_block.message
-
-        block_states = Map.put(state.block_states, block_root, initial_state)
-        blocks = Map.put(state.blocks, block_root, block)
-
-        {:noreply, state |> Map.put(:block_states, block_states) |> Map.put(:blocks, blocks)}
-
-      {:error, message} ->
-        Logger.error("[Checkpoint sync] Failed to fetch initial block: #{message}")
-        Process.send_after(self(), {:fetch_initial_block, initial_state}, 200)
-        {:noreply, state}
-    end
   end
 
   ##########################
