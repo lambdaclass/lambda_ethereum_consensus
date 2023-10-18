a45227996c33239899f53e8396ddf2b36cad712c
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2023-10-18 15:30:56 -0300

    Remove commented code in discovery config

--- native/libp2p_port/internal/discovery/discovery.go
+++ native/libp2p_port/internal/discovery/discovery.go
@@ -43,20 +43,8 @@ func NewDiscoverer(p *port.Port, listener *reqresp.Listener, config *proto_helpe
 	}
 
 	cfg := discover.Config{
-		// These settings are required and configure the UDP listener:
 		PrivateKey: privKey,
-
-		// These settings are optional:
-		// NetRestrict *netutil.Netlist  // list of allowed IP networks
-		Bootnodes: bootnodes, // list of bootstrap nodes
-		// Unhandled   chan<- ReadPacket // unhandled packets are sent on this channel
-		// Log         log.Logger        // if set, log messages go here
-
-		// V5ProtocolID configures the discv5 protocol identifier.
-		// V5ProtocolID *[6]byte
-
-		// ValidSchemes enr.IdentityScheme // allowed identity schemes
-		// Clock        mclock.Clock
+		Bootnodes:  bootnodes, // list of bootstrap nodes
 	}
 
 	db, err := enode.OpenDB("")
