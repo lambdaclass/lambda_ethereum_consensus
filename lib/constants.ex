fc95eb31650b8023897e5ac88e7e15ced057dcc8
karasakalmt <karasakal99@gmail.com>
2023-10-19 12:45:02 +0300

    added the constants for churn limit

--- lib/constants.ex
+++ lib/constants.ex
@@ -8,4 +8,10 @@ defmodule Constants do
 
   @spec timely_target_flag_index() :: integer
   def timely_target_flag_index, do: 1
+
+  @spec min_per_epoch_churn_limit() :: integer
+  def min_per_epoch_churn_limit, do: 4
+
+  @spec churn_limit_quotient() :: integer
+  def churn_limit_quotient, do: 65536
 end
