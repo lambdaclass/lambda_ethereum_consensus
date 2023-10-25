cfc9048811e796ab9cb945480fdf183c5a363574
Tomás Arjovsky <t.arjovsky@gmail.com>
2023-10-25 21:07:24 +0200

    generate minimal and mainnet configs in different directories

--- lib/mix/tasks/generate_spec_tests.ex
+++ lib/mix/tasks/generate_spec_tests.ex
@@ -3,26 +3,40 @@ defmodule Mix.Tasks.GenerateSpecTests do
   require Logger
 
   @runners ["bls", "epoch_processing", "operations", "shuffling", "ssz_static"]
+  @configs ["mainnet", "minimal"]
 
   @shortdoc "Generates tests for spec test files"
   def run(_args) do
-    @runners
-    |> Enum.each(&generate_for_runner/1)
+    for config <- @configs,
+        runner <- @runners do
+          # Create the parent dir if not present.
+          dirname = Path.join(["test", "generated", config])
+          File.mkdir_p!(dirname)
+
+          cases = SpecTestUtils.cases_for(fork: "capella", config: config, runner: runner)
+
+          if cases != [] do
+            Logger.info("Generating tests for #{config}-#{runner}.")
+            content = generate_tests(cases, config, runner)
+            File.write!(Path.join(dirname, "#{runner}.exs"), content)
+          end
+    end
   end
 
-  defp generate_for_runner(runner) do
-    Logger.info("Generating tests for #{runner}")
-    SpecTestUtils.cases_for(fork: "capella", config: "mainnet", runner: runner)
-    |> generate_tests(runner)
-    |> then(&File.write!("test/generated/#{runner}.exs", &1))
-  end
+  defp generate_tests(cases, config, runner) do
+    r = Macro.camelize(runner)
+    c = Macro.camelize(config)
 
-  defp generate_tests(cases, runner) do
-    module_name = "Elixir." <> Macro.camelize(runner) <> "Test" |> String.to_atom()
-    runner_module = "Elixir." <> Macro.camelize(runner) <> "TestRunner" |> String.to_atom()
+    module_name = "Elixir.#{c}.#{r}Test" |> String.to_atom()
+    runner_module = "Elixir.#{r}TestRunner" |> String.to_atom()
+
     header = """
     defmodule #{module_name} do
       use ExUnit.Case, async: false
+
+      setup_all do
+        Application.put_env(:lambda_ethereum_consensus, ChainSpec, config: #{chain_spec_config(config)})
+      end
     """
     footer = """
     end
@@ -40,4 +54,8 @@ defmodule Mix.Tasks.GenerateSpecTests do
       end
     """
   end
+
+  defp chain_spec_config("minimal"), do: MinimalConfig
+  defp chain_spec_config("mainnet"), do: MainnetConfig
+  defp chain_spec_config("general"), do: MainnetConfig
 end
