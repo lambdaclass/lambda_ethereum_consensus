391258ac976c43fd810195f43035e682b07f8677
karasakalmt <karasakal99@gmail.com>
2023-10-26 15:41:13 +0300

    process_attester_slashing is implemented

--- lib/lambda_ethereum_consensus/state_transition/operations.ex
+++ lib/lambda_ethereum_consensus/state_transition/operations.ex
@@ -4,8 +4,44 @@ defmodule LambdaEthereumConsensus.StateTransition.Operations do
   """
 
   alias LambdaEthereumConsensus.StateTransition.Accessors
-  alias LambdaEthereumConsensus.StateTransition.Misc
   alias LambdaEthereumConsensus.StateTransition.Predicates
+  alias LambdaEthereumConsensus.StateTransition.Mutators
   alias SszTypes.BeaconState
-  alias SszTypes.Validator
+
+  @spec process_attester_slashing(BeaconState.t(), SszTypes.AttesterSlashing.t()) ::
+          {:ok} | {:error, any()}
+  def process_attester_slashing(state, attester_slashing) do
+    attestation_1 = attester_slashing.attestation_1
+    attestation_2 = attester_slashing.attestation_2
+
+    if not Predicates.is_slashable_attestation_data(attestation_1.data, attestation_2.data),
+      do: {:error, "Attestation data is not slashable."}
+
+    if not Predicates.is_valid_indexed_attestation(state, attestation_1),
+      do: {:error, "Indexed attestation is not valid for attestation1."}
+
+    if not Predicates.is_valid_indexed_attestation(state, attestation_2),
+      do: {:error, "Indexed attestation is not valid for attestation2."}
+
+    slashed_any = false
+
+    indices =
+      Enum.uniq(attestation_1.attesting_indices)
+      |> Enum.filter(fn i -> Enum.member?(attestation_2.attesting_indices, i) end)
+      |> Enum.sort()
+      |> Enum.each(fn i ->
+        if(
+          Predicates.is_slashable_validator(
+            Enum.at(state.validators, i),
+            Accessors.get_current_epoch(state)
+          )
+        )
+
+        Mutators.slash_validator(state, i)
+        slashed_any = true
+      end)
+
+    if not slashed_any, do: {:error, "Didn't slash any."}
+    {:ok}
+  end
 end
