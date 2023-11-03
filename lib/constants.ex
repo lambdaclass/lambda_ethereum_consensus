9623d32f7a28ca3453f6ba75181cad8c208e9cb0
karasakalmt <karasakal99@gmail.com>
2023-11-03 12:33:16 +0200

    fix: removed the chain specs from constants

--- lib/constants.ex
+++ lib/constants.ex
@@ -18,15 +18,6 @@ defmodule Constants do
   @spec timely_target_flag_index() :: integer
   def timely_target_flag_index, do: 1
 
-  @spec min_per_epoch_churn_limit() :: integer
-  def min_per_epoch_churn_limit, do: 4
-
-  @spec churn_limit_quotient() :: integer
-  def churn_limit_quotient, do: 65536
-
-  @spec max_seed_lookahead() :: integer
-  def max_seed_lookahead, do: 4
-
   @spec timely_head_flag_index() :: integer
   def timely_head_flag_index, do: 2
 
@@ -54,7 +45,4 @@ defmodule Constants do
 
   @spec far_future_epoch() :: integer
   def far_future_epoch, do: 2 ** 64 - 1
-
-  @spec min_validator_withdrawability_delay() :: integer
-  def min_validator_withdrawability_delay, do: 265
 end
