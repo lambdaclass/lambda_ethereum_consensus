2bb9c139c1098040ecb28657ee169ef89cda90cb
Tomás Arjovsky <t.arjovsky@gmail.com>
2023-11-06 15:46:37 +0100

    Add query for single bit being set

--- lib/utils/bit_vector.ex
+++ lib/utils/bit_vector.ex
@@ -1,3 +1,8 @@
 defmodule LambdaEthereumConsensus.Utils.BitVector do
   def new(number, s) when is_integer(number), do: <<number::size(s)>>
+
+  def set?(bit_vector, index) do
+    skip = bit_size(bit_vector) - index - 1
+    match?(<<_::size(skip), 1::size(1), _::bitstring>>, bit_vector)
+  end
 end
--- test/unit/bit_vector_test.exs
+++ test/unit/bit_vector_test.exs
@@ -3,7 +3,15 @@ defmodule BitVectorTest do
   alias LambdaEthereumConsensus.Utils.BitVector
 
   test "build from binary" do
-    bv = BitVector.new(0b0110, 4)
-    assert bv == <<6::4>>
+    bv = BitVector.new(0b1110, 4)
+    assert bv == <<14::4>>
+  end
+
+  test "queries if a bit is set correctly using little-endian bit indexing" do
+    bv = BitVector.new(0b1110, 4)
+    assert BitVector.set?(bv, 0) == false
+    assert BitVector.set?(bv, 1) == true
+    assert BitVector.set?(bv, 2) == true
+    assert BitVector.set?(bv, 3) == true
   end
 end
