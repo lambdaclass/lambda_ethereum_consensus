2a0651ba6235162506e0f7969cc271e63996cf2a
Mete Karasakal <32202283+karasakalmt@users.noreply.github.com>
2023-11-07 14:05:35 +0200

    feat: added process_voluntary_exit (#389)

--- lib/constants.ex
+++ lib/constants.ex
@@ -12,6 +12,9 @@ defmodule Constants do
   @spec domain_beacon_proposer() :: <<_::32>>
   def domain_beacon_proposer, do: <<0, 0, 0, 0>>
 
+  @spec domain_voluntary_exit() :: <<_::32>>
+  def domain_voluntary_exit, do: <<4, 0, 0, 0>>
+
   @spec timely_source_flag_index() :: integer
   def timely_source_flag_index, do: 0
 
