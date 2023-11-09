f1b0210d486693151021dabb8062797d48d886e5
f3r10 <frledesma@outlook.com>
2023-11-09 11:20:56 -0500

    refactor ssz tests

--- test/unit/ssz_ex_test.exs
+++ test/unit/ssz_ex_test.exs
@@ -2,26 +2,24 @@ defmodule Unit.SSZExTest do
   alias LambdaEthereumConsensus.SszEx
   use ExUnit.Case
 
-  def assert_roundtrip(hex_serialized, deserialized, schema) do
-    serialized = Base.decode16!(hex_serialized)
+  def assert_roundtrip(serialized, deserialized, schema) do
     assert ^serialized = SszEx.encode(deserialized, schema)
     assert deserialized === SszEx.decode(serialized, schema)
   end
 
   test "serialize and deserialize uint" do
-    assert_roundtrip("05", 5, {:int, 8})
-    assert_roundtrip("0500", 5, {:int, 16})
-    assert_roundtrip("05000000", 5, {:int, 32})
-    assert_roundtrip("0500000000000000", 5, {:int, 64})
+    assert_roundtrip(<<5>>, 5, {:int, 8})
+    assert_roundtrip(<<5, 0>>, 5, {:int, 16})
+    assert_roundtrip(<<5, 0, 0, 0>>, 5, {:int, 32})
+    assert_roundtrip(<<5, 0, 0, 0, 0, 0, 0, 0>>, 5, {:int, 64})
 
-    assert_roundtrip("63", 99, {:int, 8})
-    assert_roundtrip("6300", 99, {:int, 16})
-    assert_roundtrip("63000000", 99, {:int, 32})
-    assert_roundtrip("6300000000000000", 99, {:int, 64})
+    assert_roundtrip(<<20, 1>>, 276, {:int, 16})
+    assert_roundtrip(<<20, 1, 0, 0>>, 276, {:int, 32})
+    assert_roundtrip(<<20, 1, 0, 0, 0, 0, 0, 0>>, 276, {:int, 64})
   end
 
   test "serialize and deserialize bool" do
-    assert_roundtrip("01", true, :bool)
-    assert_roundtrip("00", false, :bool)
+    assert_roundtrip(<<1>>, true, :bool)
+    assert_roundtrip(<<0>>, false, :bool)
   end
 end
