2916237057f3594b35270d1cf137cda71990d6ad
karasakalmt <karasakal99@gmail.com>
2023-12-05 16:38:04 +0200

    added single_merkle_proof test

--- lib/spec/runners/light_client.ex
+++ lib/spec/runners/light_client.ex
@@ -1,5 +1,6 @@
 defmodule LightClientTestRunner do
   alias LambdaEthereumConsensus.StateTransition.Predicates
+  alias LambdaEthereumConsensus.Utils.Diff
   use ExUnit.CaseTemplate
   use TestRunner
 
@@ -9,7 +10,7 @@ defmodule LightClientTestRunner do
 
   # Remove handler from here once you implement the corresponding functions
   @disabled_handlers [
-    "single_merkle_proof",
+    # "single_merkle_proof",
     "sync",
     "update_ranking",
   ]
@@ -22,14 +23,29 @@ defmodule LightClientTestRunner do
   @impl TestRunner
   def run_test_case(%SpecTestCase{} = testcase) do
     case_dir = SpecTestCase.dir(testcase)
+
+    object =
+      SpecTestUtils.read_ssz_from_file!(
+        case_dir <> "/object.ssz_snappy",
+        SszTypes.BeaconState
+      )
+
     %{leaf: leaf, leaf_index: leaf_index, branch: branch} =
       YamlElixir.read_from_file!(case_dir <> "/proof.yaml")
       |> SpecTestUtils.sanitize_yaml()
 
-    handle(testcase.handler, leaf, leaf_index, branch)
+    handle(testcase.handler, leaf, leaf_index, branch, object)
   end
 
-  defp handle("single_merkle_proof", leaf, leaf_index, branch) do
-
+  defp handle("single_merkle_proof", leaf, leaf_index, branch, object) do
+    object_root = Ssz.hash_tree_root!(object)
+    res = Predicates.is_valid_merkle_branch?(
+      leaf,
+      branch,
+      Constants.deposit_contract_tree_depth() + 1,
+      leaf_index,
+      object_root
+    )
+    assert Diff.diff(true, res) == :unchanged
   end
 end
