e7b88b63258352abd587672940ededf921ad1245
f3r10 <frledesma@outlook.com>
2023-12-06 17:31:42 -0500

    add generic containers

---
+++ lib/spec/runners/helpers/ssz_static_containers/complex_test_struct.ex
@@ -0,0 +1,46 @@
+defmodule Helpers.SszStaticContainers.ComplexTestStruct do
+  @moduledoc """
+  Struct definition for `ComplexTestStruct`.
+  """
+  alias Helpers.SszStaticContainers.FixedTestStruct
+  alias Helpers.SszStaticContainers.VarTestStruct
+  @behaviour LambdaEthereumConsensus.Container
+
+  fields = [
+    :A,
+    :B,
+    :C,
+    :D,
+    :E,
+    :F,
+    :G
+  ]
+
+  @type uint16 :: 0..unquote(2 ** 16 - 1)
+
+  @enforce_keys fields
+  defstruct fields
+
+  @type t :: %__MODULE__{
+          A: uint16(),
+          B: list(uint16()),
+          C: SszTypes.uint8(),
+          D: SszTypes.bitlist(),
+          E: VarTestStruct,
+          F: list(FixedTestStruct),
+          G: list(VarTestStruct)
+        }
+
+  @impl LambdaEthereumConsensus.Container
+  def schema do
+    [
+      {:A, {:int, 16}},
+      {:B, {:list, {:int, 16}, 1024}},
+      {:C, {:int, 8}},
+      {:D, {:bitlist, 256}},
+      {:E, VarTestStruct},
+      {:F, {:vector, FixedTestStruct, 4}},
+      {:G, {:vector, VarTestStruct, 2}}
+    ]
+  end
+end
