a297e4d9b410e1f5e7635d097a3dbf0015136303
karasakalmt <karasakal99@gmail.com>
2023-12-11 13:24:28 +0200

    metadata

---
+++ lib/lambda_ethereum_consensus/p2p/metadata.ex
@@ -0,0 +1,16 @@
+defmodule LambdaEthereumConsensus.P2P.Metadata do
+  use Genserver
+
+  ##########################
+  ### Public API
+  ##########################
+
+  ##########################
+  ### GenServer Callbacks
+  ##########################
+
+  ##########################
+  ### Private Functions
+  ##########################
+
+end
