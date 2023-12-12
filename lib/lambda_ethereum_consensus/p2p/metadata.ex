abf1dc48f50ce248aba74b118124097aeb83bb60
karasakalmt <karasakal99@gmail.com>
2023-12-12 11:53:38 +0200

    metadata genserver

--- lib/lambda_ethereum_consensus/p2p/metadata.ex
+++ lib/lambda_ethereum_consensus/p2p/metadata.ex
@@ -1,16 +1,52 @@
 defmodule LambdaEthereumConsensus.P2P.Metadata do
-  use Genserver
+  use GenServer
+
+  alias SszTypes.Metadata
+
+  @default_timeout 10_000
 
   ##########################
   ### Public API
   ##########################
 
+  @spec get_seq_number() :: SszTypes.uint64
+  def get_seq_number do
+    [seq_number] = get_metadata_attrs([:seq_number])
+    seq_number
+  end
+
+  @spec get_metadata() :: MetaData.t()
+  def get_metadata do
+    GenServer.call(__MODULE__, :get_metadata, @default_timeout)
+  end
+
   ##########################
   ### GenServer Callbacks
   ##########################
 
+  @impl GenServer
+  def init(metadata) do
+    {:ok, metadata}
+  end
+
+  @impl GenServer
+  def handle_call({:get_metadata_attrs, attrs}, _from, metadata) do
+    values = Enum.map(attrs, &Map.fetch!(metadata, &1))
+    {:reply, values, metadata}
+  end
+
+  @impl GenServer
+  def handle_call(:get_metadata, _from, metadata) do
+    {:reply, metadata}
+  end
+
   ##########################
   ### Private Functions
   ##########################
+
+  @spec get_metadata_attrs([atom()]) :: [any()]
+  defp get_metadata_attrs(attrs) do
+    GenServer.call(__MODULE__, {:get_metadata_attrs, attrs}, @default_timeout)
+  end
 
 end
