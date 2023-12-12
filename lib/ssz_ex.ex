aed341b6fad8b23239df69057b2a303c4685bd0a
Fernando Ledesma <frledesma@outlook.com>
2023-12-12 01:14:39 +0000

    feat: ssz support nested containers (#509)

--- lib/ssz_ex.ex
+++ lib/ssz_ex.ex
@@ -380,8 +380,22 @@ defmodule LambdaEthereumConsensus.SszEx do
   defp get_fixed_size({:int, size}), do: div(size, @bits_per_byte)
   defp get_fixed_size({:bytes, size}), do: size
 
+  defp get_fixed_size(module) when is_atom(module) do
+    schemas = module.schema()
+
+    schemas
+    |> Enum.map(fn {_, schema} -> get_fixed_size(schema) end)
+    |> Enum.sum()
+  end
+
   defp variable_size?({:list, _, _}), do: true
   defp variable_size?(:bool), do: false
   defp variable_size?({:int, _}), do: false
   defp variable_size?({:bytes, _}), do: false
+
+  defp variable_size?(module) when is_atom(module) do
+    module.schema()
+    |> Enum.map(fn {_, schema} -> variable_size?(schema) end)
+    |> Enum.any?()
+  end
 end
