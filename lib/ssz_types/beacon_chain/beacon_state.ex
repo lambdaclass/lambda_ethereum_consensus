e432b9b220eeba9e9a8c3497517dce5ab491513f
f3r10 <frledesma@outlook.com>
2023-12-13 18:52:38 -0500

    add schema to beacon state container

--- lib/ssz_types/beacon_chain/beacon_state.ex
+++ lib/ssz_types/beacon_chain/beacon_state.ex
@@ -3,6 +3,7 @@ defmodule SszTypes.BeaconState do
   Struct definition for `BeaconState`.
   Related definitions in `native/ssz_nif/src/types/`.
   """
+  @behaviour LambdaEthereumConsensus.Container
 
   fields = [
     :genesis_time,
@@ -46,32 +47,43 @@ defmodule SszTypes.BeaconState do
           fork: SszTypes.Fork.t(),
           # History
           latest_block_header: SszTypes.BeaconBlockHeader.t(),
+          # size SLOTS_PER_HISTORICAL_ROOT 8192
           block_roots: list(SszTypes.root()),
+          # size SLOTS_PER_HISTORICAL_ROOT 8192
           state_roots: list(SszTypes.root()),
           # Frozen in Capella, replaced by historical_summaries
+          # size HISTORICAL_ROOTS_LIMIT 16777216
           historical_roots: list(SszTypes.root()),
           # Eth1
           eth1_data: SszTypes.Eth1Data.t(),
+          # size EPOCHS_PER_ETH1_VOTING_PERIOD (64) * SLOTS_PER_EPOCH (32) = 2048
           eth1_data_votes: list(SszTypes.Eth1Data.t()),
           eth1_deposit_index: SszTypes.uint64(),
           # Registry
+          # size VALIDATOR_REGISTRY_LIMIT 1099511627776
           validators: list(SszTypes.Validator.t()),
+          # size VALIDATOR_REGISTRY_LIMIT 1099511627776
           balances: list(SszTypes.gwei()),
           # Randomness
+          # size EPOCHS_PER_HISTORICAL_VECTOR 65_536
           randao_mixes: list(SszTypes.bytes32()),
           # Slashings
           # Per-epoch sums of slashed effective balances
+          # size EPOCHS_PER_SLASHINGS_VECTOR 8192
           slashings: list(SszTypes.gwei()),
           # Participation
+          # size VALIDATOR_REGISTRY_LIMIT 1099511627776
           previous_epoch_participation: list(SszTypes.participation_flags()),
+          # size VALIDATOR_REGISTRY_LIMIT 1099511627776
           current_epoch_participation: list(SszTypes.participation_flags()),
           # Finality
-          # Bit set for every recent justified epoch
+          # Bit set for every recent justified epoch size 4
           justification_bits: SszTypes.bitvector(),
           previous_justified_checkpoint: SszTypes.Checkpoint.t(),
           current_justified_checkpoint: SszTypes.Checkpoint.t(),
           finalized_checkpoint: SszTypes.Checkpoint.t(),
           # Inactivity
+          # size VALIDATOR_REGISTRY_LIMIT 1099511627776
           inactivity_scores: list(SszTypes.uint64()),
           # Sync
           current_sync_committee: SszTypes.SyncCommittee.t(),
@@ -86,6 +98,7 @@ defmodule SszTypes.BeaconState do
           next_withdrawal_validator_index: SszTypes.withdrawal_index(),
           # Deep history valid from Capella onwards
           # [New in Capella]
+          # size HISTORICAL_ROOTS_LIMIT 16777216
           historical_summaries: list(SszTypes.HistoricalSummary.t())
         }
 
@@ -205,5 +218,39 @@ defmodule SszTypes.BeaconState do
         {0, 0}
       end
     end)
+  end
+
+  @impl LambdaEthereumConsensus.Container
+  def schema do
+    [
+      {:genesis_time, {:int, 64}},
+      {:genesis_validators_root, {:bytes, 32}},
+      {:slot, {:int, 64}},
+      {:fork, SszTypes.Fork},
+      {:latest_block_header, SszTypes.BeaconBlockHeader},
+      {:block_roots, {:list, {:bytes, 32}, 8192}},
+      {:state_roots, {:list, {:bytes, 32}, 8192}},
+      {:historical_roots, {:list, {:bytes, 32}, 16_777_216}},
+      {:eth1_data, SszTypes.Eth1Data},
+      {:eth1_data_votes, {:list, SszTypes.Eth1Data, 2048}},
+      {:eth1_deposit_index, {:int, 64}},
+      {:validators, {:list, SszTypes.Validator, 1_099_511_627_776}},
+      {:balances, {:list, {:int, 64}, 1_099_511_627_776}},
+      {:randao_mixes, {:list, {:bytes, 32}, 65_536}},
+      {:slashings, {:list, {:int, 64}, 8192}},
+      {:previous_epoch_participation, {:list, {:int, 8}, 1_099_511_627_776}},
+      {:current_epoch_participation, {:list, {:int, 8}, 1_099_511_627_776}},
+      {:justification_bits, {:bitvector, 4}},
+      {:previous_justified_checkpoint, SszTypes.Checkpoint},
+      {:current_justified_checkpoint, SszTypes.Checkpoint},
+      {:finalized_checkpoint, SszTypes.Checkpoint},
+      {:inactivity_scores, {:list, {:int, 64}, 1_099_511_627_776}},
+      {:current_sync_committee, SszTypes.SyncCommittee},
+      {:next_sync_committee, SszTypes.SyncCommittee},
+      {:latest_execution_payload_header, SszTypes.ExecutionPayloadHeader},
+      {:next_withdrawal_index, {:int, 64}},
+      {:next_withdrawal_validator_index, {:int, 64}},
+      {:historical_summaries, {:list, SszTypes.HistoricalSummary, 16_777_216}}
+    ]
   end
 end
