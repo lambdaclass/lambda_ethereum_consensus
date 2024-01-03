4e1580d415e312d1f2177a93f50abd5b221710ff
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-01-03 09:07:35 -0300

    Bump Aja version

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule LambdaEthereumConsensus.MixProject do
       {:patch, "~> 0.12.0", only: [:test]},
       {:stream_data, "~> 0.5", only: [:test]},
       {:benchee, "~> 1.2", only: [:dev]},
-      {:aja, "~> 0.6.2"},
+      {:aja, "~> 0.6"},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
