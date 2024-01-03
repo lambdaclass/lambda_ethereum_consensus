7bce9b18c972d701266b457bd4227ae206fbb1b4
karasakalmt <karasakal99@gmail.com>
2024-01-03 13:38:13 +0200

    build issue

--- lib/lambda_ethereum_consensus/p2p/metadata.ex
+++ lib/lambda_ethereum_consensus/p2p/metadata.ex
@@ -65,19 +65,21 @@ defmodule LambdaEthereumConsensus.P2P.Metadata do
 
   @impl GenServer
   def handle_cast({:set_attestation_subnet, i, set}, _from, metadata) do
-    metadata.attnets =
-      if set, do: BitVector.set(metadata.attnets, i), else: BitVector.clear(metadata.attnets, i)
-
-    metadata.seq_number = metadata.seq_number + 1
+    metadata = %Metadata{
+      metadata |
+      attnets: if set, do: BitVector.set(metadata.attnets, i), else: BitVector.clear(metadata.attnets, i)
+      seq_number: metadata.seq_number + 1
+    }
     {:noreply, metadata}
   end
 
   @impl GenServer
   def handle_cast({:set_sync_committee, i, set}, _from, metadata) do
-    metadata.syncnets =
-      if set, do: BitVector.set(metadata.syncnets, i), else: BitVector.clear(metadata.syncnets, i)
-
-    metadata.seq_number = metadata.seq_number + 1
+    metadata = %Metadata{
+      metadata |
+      syncnets: if set, do: BitVector.set(metadata.syncnets, i), else: BitVector.clear(metadata.syncnets, i)
+      seq_number: metadata.seq_number + 1
+    }
     {:noreply, metadata}
   end
 
