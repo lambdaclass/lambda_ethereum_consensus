eea65627a615b4a3aba657bec93948a247963ac2
Akash SM <akashsm@ce.iitr.ac.in>
2024-01-05 15:10:51 +0530

    add bitmask

--- lib/snappy_ex.ex
+++ lib/snappy_ex.ex
@@ -1,7 +1,11 @@
 defmodule SnappyEx do
+
+  import Bitwise
   @moduledoc """
     SSZ library in Elixir
   """
+  @bit_mask_32 2 ** 31 - 1
+  @chunk_size_limit 65_540
 
   def decompress_frames(chunks) do
     with {:ok, chunks} <- validate_stream(chunks) do
@@ -54,7 +58,7 @@ defmodule SnappyEx do
        when byte_size(chunks) < size,
        do: {:error, "invalid size: compressed data chunks"}
 
-  defp process_chunks(chunks, _acc, size, 0x00) when size > 65_540,
+  defp process_chunks(chunks, _acc, size, 0x00) when size > @chunk_size_limit,
     do: {:error, "invalid size: compressed data chunks"}
 
   defp process_chunks(chunks, acc, size, 0x00) do
@@ -68,10 +72,11 @@ defmodule SnappyEx do
 
       # the crc32 checksum of the uncompressed data is masked before inserted into the frame using masked_checksum = ((checksum >> 15) | (checksum << 17)) + 0xa282ead8
       masked_computed_checksum =
-        Bitwise.bor(Bitwise.bsr(computed_checksum, 15), Bitwise.bsl(computed_checksum, 17)) +
-          0xA282EAD8
+        computed_checksum >>> 15 |||
+          (computed_checksum <<< 17 &&& @bit_mask_32) +
+            0xA282EAD8
 
-      if computed_checksum == masked_checksum do
+      if masked_computed_checksum == masked_checksum do
         acc = <<acc::binary, decompressed_data::binary>>
         {:ok, {acc, remaining_chunks}}
       else
@@ -86,7 +91,7 @@ defmodule SnappyEx do
        do: {:error, "invalid size: uncompressed data chunks"}
 
   defp process_chunks(chunks, _acc, size, 0x01)
-       when size > 65_540,
+       when size > @chunk_size_limit,
        do: {:error, "invalid size: uncompressed data chunks"}
 
   defp process_chunks(chunks, acc, size, 0x01) do
@@ -98,10 +103,14 @@ defmodule SnappyEx do
 
     # the crc32 checksum of the uncompressed data is masked before inserted into the frame using masked_checksum = ((checksum >> 15) | (checksum << 17)) + 0xa282ead8
     masked_computed_checksum =
-      Bitwise.bor(Bitwise.bsr(computed_checksum, 15), Bitwise.bsl(computed_checksum, 17)) +
-        0xA282EAD8
+      (computed_checksum >>> (15 &&& @bit_mask_32)) |||
+        (computed_checksum <<< (17 &&& @bit_mask_32)) +
+          0xA282EAD8 &&& @bit_mask_32
 
-    if computed_checksum == masked_checksum do
+    IO.inspect(masked_checksum)
+    IO.inspect(masked_computed_checksum)
+
+    if masked_computed_checksum == masked_checksum do
       acc = <<acc::binary, uncompressed_data::binary>>
       {:ok, {acc, remaining_chunks}}
     else
