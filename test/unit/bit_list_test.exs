1655e821e06b3821b10ec59bbb3b411e11d1d223
f3r10 <frledesma@outlook.com>
2024-01-14 18:05:31 -0500

    fix bit_list tests

--- test/unit/bit_list_test.exs
+++ test/unit/bit_list_test.exs
@@ -1,46 +1,70 @@
 defmodule BitListTest do
   use ExUnit.Case
+  alias LambdaEthereumConsensus.SszEx
   alias LambdaEthereumConsensus.Utils.BitList
 
   describe "Sub-byte BitList" do
     test "build from binary" do
-      assert BitList.new(<<0b10100000, 0b1011100, 0b1>>) == {<<0b1011100, 0b10100000>>, 16}
+      input_encoded = <<237, 7>>
+      {:ok, decoded} = SszEx.decode(input_encoded, {:bitlist, 10})
+      assert BitList.set?({decoded, 10}, 0) == true
+      assert BitList.set?({decoded, 10}, 1) == false
+      assert BitList.set?({decoded, 10}, 4) == false
+      assert BitList.set?({decoded, 10}, 9) == true
+
+      {updated_bitlist, _} =
+        {decoded, 10}
+        |> BitList.set(1)
+        |> BitList.set(4)
+        |> BitList.clear(0)
+        |> BitList.clear(9)
+
+      {:ok, <<254, 5>>} = SszEx.encode(updated_bitlist, {:bitlist, 10})
     end
 
     test "sets a single bit" do
       bl = BitList.new(<<0b10100000, 0b1011100, 0b1>>)
-      assert bl |> BitList.set(0) == {<<0b1011100, 0b10100001>>, 16}
-      assert bl |> BitList.set(1) == {<<0b1011100, 0b10100010>>, 16}
-      assert bl |> BitList.set(2) == {<<0b1011100, 0b10100100>>, 16}
-      assert bl |> BitList.set(3) == {<<0b1011100, 0b10101000>>, 16}
-      assert bl |> BitList.set(4) == {<<0b1011100, 0b10110000>>, 16}
-      assert bl |> BitList.set(5) == {<<0b1011100, 0b10100000>>, 16}
-      assert bl |> BitList.set(6) == {<<0b1011100, 0b11100000>>, 16}
-      assert bl |> BitList.set(7) == {<<0b1011100, 0b10100000>>, 16}
+
+      for pos <- 0..15 do
+        assert bl
+               |> BitList.set(pos)
+               |> BitList.set?(pos)
+      end
     end
 
     test "clears a single bit" do
       bl = BitList.new(<<0b10100000, 0b1011100, 0b1>>)
-      assert bl |> BitList.clear(0) == {<<0b1011100, 0b10100000>>, 16}
-      assert bl |> BitList.clear(1) == {<<0b1011100, 0b10100000>>, 16}
-      assert bl |> BitList.clear(2) == {<<0b1011100, 0b10100000>>, 16}
-      assert bl |> BitList.clear(3) == {<<0b1011100, 0b10100000>>, 16}
-      assert bl |> BitList.clear(4) == {<<0b1011100, 0b10100000>>, 16}
-      assert bl |> BitList.clear(5) == {<<0b1011100, 0b10000000>>, 16}
-      assert bl |> BitList.clear(6) == {<<0b1011100, 0b10100000>>, 16}
-      assert bl |> BitList.clear(7) == {<<0b1011100, 0b00100000>>, 16}
+
+      for pos <- 0..15 do
+        assert bl
+               |> BitList.clear(pos)
+               |> BitList.set?(pos)
+               |> Kernel.not()
+      end
     end
 
     test "queries if a bit is set correctly using little-endian bit indexing" do
+      expected_values = [
+        false,
+        false,
+        false,
+        false,
+        false,
+        true,
+        false,
+        true,
+        false,
+        false,
+        true,
+        true,
+        true,
+        false,
+        true,
+        false
+      ]
+
       bl = BitList.new(<<0b10100000, 0b1011100, 0b1>>)
-      assert BitList.set?(bl, 0) == false
-      assert BitList.set?(bl, 1) == false
-      assert BitList.set?(bl, 2) == false
-      assert BitList.set?(bl, 3) == false
-      assert BitList.set?(bl, 4) == false
-      assert BitList.set?(bl, 5) == true
-      assert BitList.set?(bl, 6) == false
-      assert BitList.set?(bl, 7) == true
+      assert Enum.map(0..15, &BitList.set?(bl, &1)) == expected_values
     end
   end
 end
