cc26019ab196eddbb33edfe1566c21be335006d2
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-01-16 18:55:53 -0300

    Use template in configs

--- lib/chain_spec/configs/mainnet.ex
+++ lib/chain_spec/configs/mainnet.ex
@@ -2,12 +2,5 @@ defmodule MainnetConfig do
   @moduledoc """
   Mainnet config constants.
   """
-  file = "config/networks/mainnet/config.yaml"
-
-  @external_resource file
-
-  @parsed_config ConfigUtils.load_config_from_file!(file)
-  @unified Map.merge(MainnetPreset.get_preset(), @parsed_config)
-
-  def get(key), do: Map.fetch!(@unified, key)
+  use ChainSpec.GenConfig, file: "config/networks/mainnet/config.yaml", preset: MainnetPreset
 end
--- lib/chain_spec/configs/sepolia.ex
+++ lib/chain_spec/configs/sepolia.ex
@@ -2,12 +2,5 @@ defmodule SepoliaConfig do
   @moduledoc """
   Sepolia config constants.
   """
-  file = "config/networks/sepolia/config.yaml"
-
-  @external_resource file
-
-  @parsed_config ConfigUtils.load_config_from_file!(file)
-  @unified Map.merge(MainnetPreset.get_preset(), @parsed_config)
-
-  def get(key), do: Map.fetch!(@unified, key)
+  use ChainSpec.GenConfig, file: "config/networks/sepolia/config.yaml", preset: MainnetPreset
 end
