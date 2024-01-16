aa5d639e7244fbce2b87546618df270bf0e6b664
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-01-16 18:48:54 -0300

    Recompile configs and presets on file change

--- lib/chain_spec/configs/mainnet.ex
+++ lib/chain_spec/configs/mainnet.ex
@@ -2,8 +2,11 @@ defmodule MainnetConfig do
   @moduledoc """
   Mainnet config constants.
   """
+  file = "config/networks/mainnet/config.yaml"
 
-  @parsed_config ConfigUtils.load_config_from_file!("config/networks/mainnet/config.yaml")
+  @external_resource file
+
+  @parsed_config ConfigUtils.load_config_from_file!(file)
   @unified Map.merge(MainnetPreset.get_preset(), @parsed_config)
 
   def get(key), do: Map.fetch!(@unified, key)
