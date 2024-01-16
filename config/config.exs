d892a7d51eb36e3f0e9f574369fb388e295d6431
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-01-16 15:24:33 -0300

    Add file logging config as comment

--- config/config.exs
+++ config/config.exs
@@ -13,6 +13,18 @@ config :lambda_ethereum_consensus, BeaconApi.Endpoint,
     layout: false
   ]
 
+## TODO: we might want to enable this with a CLI flag
+## Uncomment to log to a file
+# config :logger, :default_handler,
+#   config: [
+#     file: ~c"logs/system.log",
+#     filesync_repeat_interval: 5000,
+#     file_check: 5000,
+#     max_no_bytes: 10_000_000,
+#     max_no_files: 5,
+#     compress_on_rotate: true
+#   ]
+
 config :lambda_ethereum_consensus, LambdaEthereumConsensus.Telemetry, enable: true
 
 # Use Jason for JSON parsing in Phoenix
