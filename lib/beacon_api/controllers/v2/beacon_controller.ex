74e801d27a918b5f7fd5ce9d13c74a92506015f0
f3r10 <frledesma@outlook.com>
2024-01-16 20:12:50 -0500

    add api operation to controller v2

--- lib/beacon_api/controllers/v2/beacon_controller.ex
+++ lib/beacon_api/controllers/v2/beacon_controller.ex
@@ -1,30 +1,41 @@
 defmodule BeaconApi.V2.BeaconController do
+  alias BeaconApi.ApiSpec
   alias BeaconApi.ErrorController
   alias LambdaEthereumConsensus.Store.BlockStore
   use BeaconApi, :controller
 
+  plug(OpenApiSpex.Plug.CastAndValidate, json_render_error_v2: true)
+
+  def open_api_operation(action) do
+    IO.puts("action #{inspect(action)}")
+    apply(__MODULE__, :"#{action}_operation", [])
+  end
+
+  def get_block_operation,
+    do: ApiSpec.spec().paths["/eth/v2/beacon/blocks/{block_id}"].get
+
   @spec get_block(Plug.Conn.t(), any) :: Plug.Conn.t()
-  def get_block(conn, %{"block_id" => "head"}) do
+  def get_block(conn, %{block_id:  "head"}) do
     # TODO: determine head and return it
     conn |> block_not_found()
   end
 
-  def get_block(conn, %{"block_id" => "finalized"}) do
+  def get_block(conn, %{block_id: "finalized"}) do
     # TODO
     conn |> block_not_found()
   end
 
-  def get_block(conn, %{"block_id" => "justified"}) do
+  def get_block(conn, %{block_id: "justified"}) do
     # TODO
     conn |> block_not_found()
   end
 
-  def get_block(conn, %{"block_id" => "genesis"}) do
+  def get_block(conn, %{block_id: "genesis"}) do
     # TODO
     conn |> block_not_found()
   end
 
-  def get_block(conn, %{"block_id" => "0x" <> hex_block_id}) do
+  def get_block(conn, %{block_id: "0x" <> hex_block_id}) do
     with {:ok, block_root} <- Base.decode16(hex_block_id, case: :mixed),
          {:ok, block} <- BlockStore.get_block(block_root) do
       conn |> block_response(block)
@@ -34,7 +45,7 @@ defmodule BeaconApi.V2.BeaconController do
     end
   end
 
-  def get_block(conn, %{"block_id" => block_id}) do
+  def get_block(conn, %{block_id: block_id}) do
     with {slot, ""} when slot >= 0 <- Integer.parse(block_id),
          {:ok, block} <- BlockStore.get_block_by_slot(slot) do
       conn |> block_response(block)
