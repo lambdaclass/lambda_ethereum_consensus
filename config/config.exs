e3e9be9c161da914fc16ede60ee41ca5314d3611
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-01-18 14:23:36 -0300

    feat: add loki to docker-compose (#639)

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,18 @@ import Config
 # Configure logging
 config :logger, level: :info, truncate: :infinity
 
+## TODO: we might want to enable this with a CLI flag
+## Uncomment to log to a file
+# config :logger, :default_handler,
+#   config: [
+#     file: ~c"logs/system.log",
+#     filesync_repeat_interval: 5000,
+#     file_check: 5000,
+#     max_no_bytes: 10_000_000,
+#     max_no_files: 5,
+#     compress_on_rotate: true
+#   ]
+
 # Configures the phoenix endpoint
 config :lambda_ethereum_consensus, BeaconApi.Endpoint,
   http: [port: 4000],
