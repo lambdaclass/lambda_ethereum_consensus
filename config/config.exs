2086d04655135d712a5b3fd04c7409ff29d0f756
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-02-09 14:24:07 -0300

    feat: add flag for logging to a file (#735)

--- config/config.exs
+++ config/config.exs
@@ -4,32 +4,10 @@ import Config
 # Configure logging
 config :logger, level: :info, truncate: :infinity
 
-config :logger, :default_formatter, format: {ConsoleLogger, :format}, metadata: [:slot, :root]
+config :lambda_ethereum_consensus, LambdaEthereumConsensus.Telemetry, enable: true
 
-# # Uncomment to log to a file
-# # TODO: we might want to enable this with a CLI flag
-# config :logger, :default_handler,
-#   config: [
-#     file: ~c"logs/system.log",
-#     filesync_repeat_interval: 5000,
-#     file_check: 5000,
-#     max_no_bytes: 10_000_000,
-#     max_no_files: 5,
-#     compress_on_rotate: true
-#   ]
-
-# # NOTE: We want to log UTC timestamps, for convenience
-# config :logger, utc_log: true
-
-# config :logger, :default_formatter,
-#   format: {LogfmtEx, :format},
-#   colors: [enabled: false],
-#   metadata: [:mfa]
-
-# config :logfmt_ex, :opts,
-#   message_key: "msg",
-#   timestamp_key: "ts",
-#   timestamp_format: :iso8601
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
 
 # Configures the phoenix endpoint
 config :lambda_ethereum_consensus, BeaconApi.Endpoint,
@@ -39,8 +17,3 @@ config :lambda_ethereum_consensus, BeaconApi.Endpoint,
     formats: [json: BeaconApi.ErrorJSON],
     layout: false
   ]
-
-config :lambda_ethereum_consensus, LambdaEthereumConsensus.Telemetry, enable: true
-
-# Use Jason for JSON parsing in Phoenix
-config :phoenix, :json_library, Jason
