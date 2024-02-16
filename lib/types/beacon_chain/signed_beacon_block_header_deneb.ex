b9ec60df63db355a28bb2ac09cf7fc3ba5ef2ce2
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-02-16 18:08:32 -0300

    Add new Elixir types

---
+++ lib/types/beacon_chain/signed_beacon_block_header_deneb.ex
@@ -0,0 +1,28 @@
+defmodule Types.SignedBeaconBlockHeaderDeneb do
+  @moduledoc """
+  Struct definition for `SignedBeaconBlockHeader`.
+  Related definitions in `native/ssz_nif/src/types/`.
+  """
+  @behaviour LambdaEthereumConsensus.Container
+
+  fields = [
+    :message,
+    :signature
+  ]
+
+  @enforce_keys fields
+  defstruct fields
+
+  @type t :: %__MODULE__{
+          message: Types.BeaconBlockHeaderDeneb.t(),
+          signature: Types.bls_signature()
+        }
+
+  @impl LambdaEthereumConsensus.Container
+  def schema do
+    [
+      {:message, Types.BeaconBlockHeaderDeneb},
+      {:signature, TypeAliases.bls_signature()}
+    ]
+  end
+end
