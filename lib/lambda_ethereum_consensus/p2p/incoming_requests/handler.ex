e79d4c00d3af14cbc8828401637a4b2d02c5e470
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-02-21 16:57:12 -0300

    Simplify decoding

--- lib/lambda_ethereum_consensus/p2p/incoming_requests/handler.ex
+++ lib/lambda_ethereum_consensus/p2p/incoming_requests/handler.ex
@@ -25,18 +25,18 @@ defmodule LambdaEthereumConsensus.P2P.IncomingRequests.Handler do
   defp handle_req(protocol_name, message_id, message)
 
   defp handle_req("status/1/ssz_snappy", message_id, message) do
-    with {:ok, request} <- ReqResp.decode_request(message, Types.StatusMessage, 84),
+    with {:ok, request} <- ReqResp.decode(message, Types.StatusMessage),
          Logger.debug("[Status] '#{inspect(request)}'"),
          {:ok, current_status} <- BeaconChain.get_current_status_message(),
-         {:ok, payload} <- ReqResp.encode_response(current_status) do
+         {:ok, payload} <- ReqResp.encode_ok(current_status) do
       Libp2pPort.send_response(message_id, payload)
     end
   end
 
   defp handle_req("goodbye/1/ssz_snappy", message_id, message) do
-    with {:ok, goodbye_reason} <- ReqResp.decode_request(message, TypeAliases.uint64(), 8),
+    with {:ok, goodbye_reason} <- ReqResp.decode(message, TypeAliases.uint64()),
          Logger.debug("[Goodbye] reason: #{goodbye_reason}"),
-         {:ok, payload} <- ReqResp.encode_response({0, TypeAliases.uint64()}) do
+         {:ok, payload} <- ReqResp.encode_ok({0, TypeAliases.uint64()}) do
       Libp2pPort.send_response(message_id, payload)
     else
       # Ignore read errors, since some peers eagerly disconnect.
@@ -55,23 +55,23 @@ defmodule LambdaEthereumConsensus.P2P.IncomingRequests.Handler do
 
   defp handle_req("ping/1/ssz_snappy", message_id, message) do
     # Values are hardcoded
-    with {:ok, seq_num} <- ReqResp.decode_request(message, TypeAliases.uint64(), 8),
+    with {:ok, seq_num} <- ReqResp.decode(message, TypeAliases.uint64()),
          Logger.debug("[Ping] seq_number: #{seq_num}"),
          seq_number = P2P.Metadata.get_seq_number(),
-         {:ok, payload} <- ReqResp.encode_response({seq_number, TypeAliases.uint64()}) do
+         {:ok, payload} <- ReqResp.encode_ok({seq_number, TypeAliases.uint64()}) do
       Libp2pPort.send_response(message_id, payload)
     end
   end
 
   defp handle_req("metadata/2/ssz_snappy", message_id, _message) do
     # NOTE: there's no request content so we just ignore it
-    with {:ok, payload} <- P2P.Metadata.get_metadata() |> ReqResp.encode_response() do
+    with {:ok, payload} <- P2P.Metadata.get_metadata() |> ReqResp.encode_ok() do
       Libp2pPort.send_response(message_id, payload)
     end
   end
 
   defp handle_req("beacon_blocks_by_range/2/ssz_snappy", message_id, message) do
-    with {:ok, request} <- ReqResp.decode_request(message, Types.BeaconBlocksByRangeRequest, 24) do
+    with {:ok, request} <- ReqResp.decode(message, Types.BeaconBlocksByRangeRequest) do
       %{start_slot: start_slot, count: count} = request
 
       Logger.info("[BlocksByRange] requested #{count} slots, starting from #{start_slot}")
@@ -85,15 +85,14 @@ defmodule LambdaEthereumConsensus.P2P.IncomingRequests.Handler do
         |> Enum.map(&BlockDb.get_block_by_slot/1)
         |> Enum.map(&map_block_result/1)
         |> Enum.reject(&(&1 == :skip))
-        |> ReqResp.encode_response_chunks()
+        |> ReqResp.encode_ok_chunks()
 
       Libp2pPort.send_response(message_id, response_chunk)
     end
   end
 
   defp handle_req("beacon_blocks_by_root/2/ssz_snappy", message_id, message) do
-    with {:ok, %{body: body}} <-
-           ReqResp.decode_request(message, Types.BeaconBlocksByRootRequest, 24) do
+    with {:ok, %{body: body}} <- ReqResp.decode(message, Types.BeaconBlocksByRootRequest) do
       count = length(body)
       Logger.info("[BlocksByRoot] requested #{count} number of blocks")
       truncated_count = min(count, ChainSpec.get("MAX_REQUEST_BLOCKS"))
@@ -104,7 +103,7 @@ defmodule LambdaEthereumConsensus.P2P.IncomingRequests.Handler do
         |> Enum.map(&Blocks.get_signed_block/1)
         |> Enum.map(&map_block_result/1)
         |> Enum.reject(&(&1 == :skip))
-        |> ReqResp.encode_response_chunks()
+        |> ReqResp.encode_ok_chunks()
 
       Libp2pPort.send_response(message_id, response_chunk)
     end
