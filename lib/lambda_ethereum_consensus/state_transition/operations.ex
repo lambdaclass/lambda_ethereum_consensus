d7dee375493180539dcd66ae89736aef1d87370a
f3r10 <frledesma@outlook.com>
2024-02-27 17:12:36 -0500

    feat: update process_voluntary_exit according to EIP-7044

--- lib/lambda_ethereum_consensus/state_transition/operations.ex
+++ lib/lambda_ethereum_consensus/state_transition/operations.ex
@@ -553,7 +553,11 @@ defmodule LambdaEthereumConsensus.StateTransition.Operations do
       current_epoch < validator.activation_epoch + ChainSpec.get("SHARD_COMMITTEE_PERIOD") ->
         {:error, "validator cannot exit yet"}
 
-      not (Accessors.get_domain(state, Constants.domain_voluntary_exit(), voluntary_exit.epoch)
+      not (Misc.compute_domain(
+             Constants.domain_voluntary_exit(),
+             fork_version: ChainSpec.get("CAPELLA_FORK_VERSION"),
+             genesis_validators_root: state.genesis_validators_root
+           )
            |> then(&Misc.compute_signing_root(voluntary_exit, &1))
            |> then(&Bls.valid?(validator.pubkey, &1, signed_voluntary_exit.signature))) ->
         {:error, "invalid signature"}
