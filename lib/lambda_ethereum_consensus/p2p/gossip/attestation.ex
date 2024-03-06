fe65e8db904c25de643b6ea60246504c75e84c7d
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-03-06 09:36:36 -0300

    Update ENR

--- lib/lambda_ethereum_consensus/p2p/gossip/attestation.ex
+++ lib/lambda_ethereum_consensus/p2p/gossip/attestation.ex
@@ -13,16 +13,18 @@ defmodule LambdaEthereumConsensus.P2P.Gossip.Attestation do
 
   def join(subnet_id) do
     topic = get_topic_name(subnet_id)
-    # TODO: update ENR
     Libp2pPort.join_topic(topic)
     P2P.Metadata.set_attestation_subnet(subnet_id, true)
+    # NOTE: this depends on the metadata being updated
+    update_enr()
   end
 
   def leave(subnet_id) do
     topic = get_topic_name(subnet_id)
-    # TODO: update ENR
     Libp2pPort.leave_topic(topic)
     P2P.Metadata.set_attestation_subnet(subnet_id, false)
+    # NOTE: this depends on the metadata being updated
+    update_enr()
   end
 
   defp extract_subnet_id(topic) do
@@ -38,5 +40,27 @@ defmodule LambdaEthereumConsensus.P2P.Gossip.Attestation do
   defp store_attestation(subnet_id, %{attestations: attestations} = state, attestation) do
     attestations = Map.update(attestations, subnet_id, [], &[attestation | &1])
     %{state | attestations: attestations}
+  end
+
+  defp update_enr do
+    enr_fork_id = compute_enr_fork_id()
+    %{attnets: attnets, syncnets: syncnets} = P2P.Metadata.get_metadata()
+    Libp2pPort.update_enr(enr_fork_id, attnets, syncnets)
+  end
+
+  defp compute_enr_fork_id do
+    current_version = BeaconChain.get_fork_version()
+
+    fork_digest =
+      Misc.compute_fork_digest(current_version, ChainSpec.get_genesis_validators_root())
+
+    attnets = ChainSpec.get("ATTESTATION_SUBNET_COUNT") |> BitVector.new()
+    syncnets = Constants.sync_committee_subnet_count() |> BitVector.new()
+
+    %EnrForkId{
+      fork_digest: fork_digest,
+      next_fork_version: current_version,
+      next_fork_epoch: Constants.far_future_epoch()
+    }
   end
 end
