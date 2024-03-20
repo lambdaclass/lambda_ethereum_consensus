6176f8d28fdd937b2fe066f0f317ef42afe1e460
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-03-20 15:00:23 -0300

    Change `:prometheus_process_collector` to fork
    A Makefile used to compile the c_src in the original repo had a bug.
    I already forked it and made a PR fixing the bug, but in the meantime we should just reference that fork.

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule LambdaEthereumConsensus.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :observer, :prometheus_ex, :prometheus_plugs],
+      extra_applications: [:logger, :observer, :prometheus_ex],
       mod: {LambdaEthereumConsensus.Application, []}
     ]
   end
@@ -63,7 +63,11 @@ defmodule LambdaEthereumConsensus.MixProject do
       {:crc32c, git: "https://github.com/lambdaclass/crc32c", branch: "bump-rustler-31"},
       {:sentry, "~> 10.2.0"},
       {:prometheus_ex, "~> 3.1"},
-      {:prometheus_plugs, "~> 1.1"}
+      {:prometheus_plugs, "~> 1.1"},
+      {:prometheus_process_collector,
+       git: "https://github.com/lambdaclass/prometheus_process_collector",
+       branch: "update-makefile-to-support-otp-26",
+       override: true}
     ]
   end
 
