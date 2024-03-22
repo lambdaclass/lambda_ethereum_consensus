c35aebf08960f09a1c6876a77bdf0a9b0f29413c
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-03-22 15:54:53 -0300

    feat: collect BLSToExecutionChange from gossip

---
+++ lib/lambda_ethereum_consensus/p2p/gossip/operations_collector.ex
@@ -0,0 +1,46 @@
+defmodule LambdaEthereumConsensus.P2P.Gossip.OperationsCollector do
+  @moduledoc """
+  Module that stores the operations received from gossipsub.
+  """
+  alias Types.SignedBeaconBlock
+  alias Types.SignedBLSToExecutionChange
+
+  use GenServer
+
+  def start_link(opts), do: GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+
+  def notify_bls_to_execution_change_gossip(%SignedBLSToExecutionChange{} = msg) do
+    GenServer.cast(__MODULE__, {:bls_to_execution_change, msg})
+  end
+
+  def notify_new_block(%SignedBeaconBlock{} = block) do
+    GenServer.cast(__MODULE__, {:new_block, block})
+  end
+
+  @impl GenServer
+  def init(_init_arg) do
+    {:ok, %{bls_to_execution_change: []}}
+  end
+
+  @impl GenServer
+  def handle_cast({:bls_to_execution_change, msg}, state) do
+    new_msgs = [msg | state.bls_to_execution_change]
+    {:noreply, %{state | bls_to_execution_change: new_msgs}}
+  end
+
+  def handle_cast({:new_block, block}, state) do
+    {:noreply, filter_messages(state, block)}
+  end
+
+  defp filter_messages(state, block) do
+    indices =
+      block.message.body.bls_to_execution_changes
+      |> MapSet.new(& &1.message.validator_index)
+
+    bls_to_execution_changes =
+      state.bls_to_execution_change
+      |> Enum.reject(&MapSet.member?(indices, &1.message.validator_index))
+
+    %{state | bls_to_execution_change: bls_to_execution_changes}
+  end
+end
