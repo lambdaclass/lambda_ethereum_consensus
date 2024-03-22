d67239c13030b7db4b592e7ae67fc6a98aa535d7
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-03-22 14:24:10 -0300

    fix: was subscribing in the wrong process

--- lib/lambda_ethereum_consensus/p2p/gossip/attestation.ex
+++ lib/lambda_ethereum_consensus/p2p/gossip/attestation.ex
@@ -55,7 +55,6 @@ defmodule LambdaEthereumConsensus.P2P.Gossip.Attestation do
   def collect(subnet_id, attestation) do
     GenServer.call(__MODULE__, {:collect, subnet_id, attestation})
     join(subnet_id)
-    get_topic_name(subnet_id) |> Libp2pPort.subscribe_to_topic()
   end
 
   @spec stop_collecting(non_neg_integer()) ::
@@ -100,6 +99,7 @@ defmodule LambdaEthereumConsensus.P2P.Gossip.Attestation do
     attestations = Map.put(state.attestations, subnet_id, [attestation])
     attnets = Map.put(state.attnets, subnet_id, attestation.data)
     new_state = %{state | attnets: attnets, attestations: attestations}
+    get_topic_name(subnet_id) |> Libp2pPort.subscribe_to_topic()
     {:reply, :ok, new_state}
   end
 
