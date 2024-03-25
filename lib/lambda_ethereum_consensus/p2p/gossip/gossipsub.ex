e3cebfd2bad84437f9555e7696b887f9ec8ca3e2
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-03-25 15:04:17 -0300

    feat: collect slashings and voluntary exits (#927)

--- lib/lambda_ethereum_consensus/p2p/gossip/gossipsub.ex
+++ lib/lambda_ethereum_consensus/p2p/gossip/gossipsub.ex
@@ -19,9 +19,9 @@ defmodule LambdaEthereumConsensus.P2P.GossipSub do
       {"beacon_block", SignedBeaconBlock, &Handler.handle_beacon_block/1},
       {"beacon_aggregate_and_proof", Types.SignedAggregateAndProof,
        &Handler.handle_beacon_aggregate_and_proof/1},
-      # {"voluntary_exit", Types.SignedVoluntaryExit},
-      # {"proposer_slashing", Types.ProposerSlashing},
-      # {"attester_slashing", Types.AttesterSlashing},
+      {"voluntary_exit", Types.SignedVoluntaryExit, &Handler.handle_voluntary_exit/1},
+      {"proposer_slashing", Types.ProposerSlashing, &Handler.handle_proposer_slashing/1},
+      {"attester_slashing", Types.AttesterSlashing, &Handler.handle_attester_slashing/1},
       {"bls_to_execution_change", Types.SignedBLSToExecutionChange,
        &Handler.handle_bls_to_execution_change/1}
       # {"sync_committee_contribution_and_proof", Types.SignedContributionAndProof},
