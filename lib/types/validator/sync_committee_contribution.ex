a65c2abe5ab69b096df7f27f8577952389004261
avilagaston9 <gavila@fi.uba.ar>
2024-03-28 14:09:51 -0300

    fix: add encode and decode for SyncCommitteeContribution

--- lib/types/validator/sync_committee_contribution.ex
+++ lib/types/validator/sync_committee_contribution.ex
@@ -4,6 +4,7 @@ defmodule Types.SyncCommitteeContribution do
   Related definitions in `native/ssz_nif/src/types/`.
   """
   use LambdaEthereumConsensus.Container
+  alias LambdaEthereumConsensus.Utils.BitVector
 
   fields = [
     :slot,
@@ -38,5 +39,24 @@ defmodule Types.SyncCommitteeContribution do
         )}},
       {:signature, TypeAliases.bls_signature()}
     ]
+  end
+
+  def encode(%__MODULE__{} = map) do
+    # NOTE: we do this because the SSZ NIF cannot decode bitstrings
+    # TODO: remove when migrating to the new SSZ lib
+    map
+    |> Map.update!(:aggregation_bits, &BitVector.to_bytes/1)
+  end
+
+  def decode(%__MODULE__{} = map) do
+    # NOTE: this isn't really needed
+    aggregation_bits_count =
+      div(
+        ChainSpec.get("SYNC_COMMITTEE_SIZE"),
+        Constants.sync_committee_subnet_count()
+      )
+
+    map
+    |> Map.update!(:aggregation_bits, &BitVector.new(&1, aggregation_bits_count))
   end
 end
