f69dd66d8756abed7a38b0bef877d560efe1d951
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-04-05 12:24:44 -0300

    Compute the eth1 vote on validated blocks

--- lib/lambda_ethereum_consensus/execution/eth1_chain.ex
+++ lib/lambda_ethereum_consensus/execution/eth1_chain.ex
@@ -6,14 +6,17 @@ defmodule LambdaEthereumConsensus.Execution.Eth1Chain do
   alias Types.ExecutionPayload
   use GenServer
 
+  @spec start_link(Types.uint64()) :: GenServer.on_start()
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
-  def get_eth1_vote do
-    GenServer.call(__MODULE__, :get_eth1_vote)
+  @spec get_eth1_vote(Types.slot()) :: Types.Eth1Data.t() | nil
+  def get_eth1_vote(slot) do
+    GenServer.call(__MODULE__, {:get_eth1_vote, slot})
   end
 
+  @spec notify_new_block(Types.slot(), Types.Eth1Data.t(), ExecutionPayload.t()) :: :ok
   def notify_new_block(slot, eth1_data, %ExecutionPayload{} = execution_payload) do
     payload_info = Map.take(execution_payload, [:block_hash, :block_number, :timestamp])
     GenServer.cast(__MODULE__, {:new_block, slot, eth1_data, payload_info})
@@ -26,32 +29,36 @@ defmodule LambdaEthereumConsensus.Execution.Eth1Chain do
   end
 
   @impl true
-  def handle_call(:get_eth1_vote, _from, state) do
-    {:reply, compute_eth1_vote(state), state}
+  def handle_call({:get_eth1_vote, slot}, _from, state) do
+    {:reply, compute_eth1_vote(state, slot), state}
   end
 
   @impl true
   def handle_cast({:new_block, slot, eth1_data, payload_info}, state) do
     state
-    |> update_state_with_payload(slot, payload_info)
-    |> update_state_with_vote(slot, eth1_data)
+    |> prune_state(slot)
+    |> update_state_with_payload(payload_info)
+    |> update_state_with_vote(eth1_data)
     |> then(&{:noreply, &1})
   end
 
-  defp update_state_with_payload(slot, state, payload_info) do
-    %{genesis_time: genesis_time, eth1_chain: eth1_chain, last_period: last_period} = state
+  defp prune_state(%{genesis_time: genesis_time, last_period: last_period} = state, slot) do
     current_period = compute_period(slot)
-    new_eth1_chain = eth1_chain |> drop_old_blocks(genesis_time, current_period, last_period)
-    %{state | eth1_chain: [payload_info | new_eth1_chain], last_period: current_period}
+
+    if current_period > last_period do
+      new_chain = drop_old_payloads(state.eth1_chain, genesis_time, slot)
+      %{state | eth1_data_votes: [], eth1_chain: new_chain, last_period: current_period}
+    else
+      state
+    end
   end
 
-  defp drop_old_blocks(eth1_chain, _, current_period, last_period)
-       when current_period == last_period do
-    eth1_chain
+  defp update_state_with_payload(%{eth1_chain: eth1_chain} = state, payload_info) do
+    %{state | eth1_chain: [payload_info | eth1_chain]}
   end
 
-  defp drop_old_blocks(eth1_chain, genesis_time, current_period, _) do
-    period_start = voting_period_start_time(current_period, genesis_time)
+  defp drop_old_payloads(eth1_chain, genesis_time, slot) do
+    period_start = voting_period_start_time(slot, genesis_time)
 
     follow_time_distance =
       ChainSpec.get("SECONDS_PER_ETH1_BLOCK") * ChainSpec.get("ETH1_FOLLOW_DISTANCE")
@@ -61,26 +68,49 @@ defmodule LambdaEthereumConsensus.Execution.Eth1Chain do
     Enum.take_while(eth1_chain, fn %{timestamp: timestamp} -> timestamp >= cutoff_time end)
   end
 
-  defp update_state_with_vote(state, _slot, eth1_data) do
+  defp update_state_with_vote(state, eth1_data) do
     # NOTE: votes are in order of appearance
     eth1_data_votes = Keyword.update(state.eth1_data_votes, eth1_data, 0, &(&1 + 1))
     %{state | eth1_data_votes: eth1_data_votes}
   end
 
-  defp compute_eth1_vote(%{eth1_data_votes: []}), do: nil
+  defp compute_eth1_vote(%{eth1_data_votes: []}, _), do: nil
 
-  defp compute_eth1_vote(state) do
-    # TODO:
-    # 1. validate each of the votes against the known eth1 chain
-    #   a. the eth1 data should be from a candidate block
-    #   b. the deposit count should be greater than or equal to the current eth1 data's
-    # 2. filter out the invalid votes
-    # 3. Default vote on latest eth1 block data in the period range unless eth1 chain is not live
-    state.eth1_data_votes |> Enum.max(&(elem(&1, 1) >= elem(&2, 1))) |> elem(0)
+  defp compute_eth1_vote(
+         %{eth1_chain: eth1_chain, eth1_data_votes: seen_votes, genesis_time: genesis_time},
+         slot
+       ) do
+    period_start = voting_period_start_time(slot, genesis_time)
+    follow_time = ChainSpec.get("SECONDS_PER_ETH1_BLOCK") * ChainSpec.get("ETH1_FOLLOW_DISTANCE")
+
+    # TODO: get the eth1 data (deposit_root, deposit_count) for each block
+    blocks_to_consider =
+      Enum.filter(eth1_chain, &candidate_block?(&1.timestamp, period_start, follow_time))
+
+    result =
+      seen_votes
+      |> Stream.map(fn {%{block_hash: hash} = eth1_data, count} ->
+        {eth1_data, count, Enum.find_index(blocks_to_consider, &(hash == &1.block_hash))}
+      end)
+      |> Stream.reject(&is_nil(elem(&1, 2)))
+      |> Enum.max(
+        fn {_, count1, index1}, {_, count2, index2} -> {count1, -index1} >= {count2, -index2} end,
+        nil
+      )
+
+    case result do
+      nil -> nil
+      {eth1_data, _, _} -> eth1_data
+    end
   end
 
-  defp voting_period_start_time(period, genesis_time) do
-    period_start_slot = period * slots_per_eth1_voting_period()
+  defp candidate_block?(timestamp, period_start, follow_time) do
+    # follow_time = SECONDS_PER_ETH1_BLOCK * ETH1_FOLLOW_DISTANCE
+    timestamp in (period_start - follow_time * 2)..(period_start - follow_time)
+  end
+
+  defp voting_period_start_time(slot, genesis_time) do
+    period_start_slot = slot - rem(slot, slots_per_eth1_voting_period())
     genesis_time + period_start_slot * ChainSpec.get("SECONDS_PER_SLOT")
   end
 
@@ -88,9 +118,4 @@ defmodule LambdaEthereumConsensus.Execution.Eth1Chain do
 
   defp slots_per_eth1_voting_period,
     do: ChainSpec.get("EPOCHS_PER_ETH1_VOTING_PERIOD") * ChainSpec.get("SLOTS_PER_EPOCH")
-
-  # def is_candidate_block(block: Eth1Block, period_start: uint64) -> bool:
-  # return (
-  #     period_start - SECONDS_PER_ETH1_BLOCK * ETH1_FOLLOW_DISTANCE * 2 <= block.timestamp <= period_start - SECONDS_PER_ETH1_BLOCK * ETH1_FOLLOW_DISTANCE
-  # )
 end
