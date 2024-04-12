3c6afa56af6679d669ecc79a5cf27df3ce15b860
avilagaston9 <gavila@fi.uba.ar>
2024-04-12 12:29:50 -0300

    Add benchmark for hash_tree_root

--- bench/ssz_ex_bench.exs
+++ bench/ssz_ex_bench.exs
@@ -1,45 +1,36 @@
 alias LambdaEthereumConsensus.SszEx
+alias LambdaEthereumConsensus.Store.StateDb
 alias Types.BeaconState
 
-{:ok, encoded_state} = File.read!("./bench/beacon_state.ssz.snappy") |> Snappy.decompress()
+# To run this benchmarks, checkpoint-sync is needed.
 
+{:ok, state} = StateDb.get_latest_state()
 :mainnet = ChainSpec.get_preset()
+{:ok, encoded_state} = Ssz.to_ssz(state)
 
-{:ok, state} = Ssz.from_ssz(encoded_state, BeaconState)
+Benchee.run(
+  %{
+    "SszEx.decode" => fn -> SszEx.decode(encoded_state, BeaconState) end,
+    "Ssz.from_ssz" => fn -> Ssz.from_ssz(encoded_state, BeaconState) end
+  },
+  warmup: 2,
+  time: 5
+)
 
-# Benchee.run(
-#   %{
-#     "SszEx.decode" => fn -> SszEx.decode(encoded_state, BeaconState) end,
-#     "Ssz.from_ssz" => fn -> Ssz.from_ssz(encoded_state, BeaconState) end
-#   },
-#   after_each: fn result ->
-#     {:ok, _decoded} = result
-#   end,
-#   warmup: 2,
-#   time: 5
-# )
-
-# Benchee.run(
-#   %{
-#     "SszEx.encode" => fn -> SszEx.encode(state, BeaconState) end,
-#     "Ssz.to_ssz" => fn -> Ssz.to_ssz(state) end
-#   },
-#   after_each: fn result ->
-#     {:ok, _encoded} = result
-#   end,
-#   warmup: 2,
-#   time: 5
-# )
+Benchee.run(
+  %{
+    "SszEx.encode" => fn -> SszEx.encode(state, BeaconState) end,
+    "Ssz.to_ssz" => fn -> Ssz.to_ssz(state) end
+  },
+  warmup: 2,
+  time: 5
+)
 
 Benchee.run(
   %{
     "SszEx.hash_tree_root!" => fn -> SszEx.hash_tree_root!(state, BeaconState) end,
     "Ssz.hash_tree_root" => fn -> Ssz.hash_tree_root(state, BeaconState) end
   },
-  after_each: fn result ->
-    {:ok, encoded} = result
-    dbg(encoded |> Base.encode16())
-  end,
   warmup: 2,
-  time: 5
+  time: 30
 )
