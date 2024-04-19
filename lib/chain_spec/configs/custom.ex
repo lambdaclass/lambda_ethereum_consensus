b87159e98f1ef969fadc35a40933d3f19c7e74ae
Tomás Arjovsky <t.arjovsky@gmail.com>
2024-04-19 20:23:30 +0100

    feat: allow genesis from file for local testnets (#946)
    Co-authored-by: Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>

--- lib/chain_spec/configs/custom.ex
+++ lib/chain_spec/configs/custom.ex
@@ -8,13 +8,33 @@ defmodule CustomConfig do
   def load_from_file!(path) do
     config = ConfigUtils.load_config_from_file!(path)
     preset = Map.fetch!(config, "PRESET_BASE") |> ConfigUtils.parse_preset()
-    merged_config = Map.merge(preset.get_preset(), config)
+    base_config = Map.fetch!(config, "CONFIG_NAME") |> ConfigUtils.parse_config()
+
+    merged_config =
+      preset.get_preset()
+      |> Map.merge(base_config.get_all())
+      |> Map.merge(config)
+
     Application.put_env(:lambda_ethereum_consensus, __MODULE__, merged: merged_config)
   end
 
-  defp get_config,
-    do: Application.get_env(:lambda_ethereum_consensus, __MODULE__) |> Keyword.fetch!(:merged)
+  @impl GenConfig
+  def get_all do
+    Application.get_env(:lambda_ethereum_consensus, __MODULE__)
+    |> Keyword.fetch!(:merged)
+    |> Map.new(fn {k, v} -> {k, parse_int(v)} end)
+  end
 
   @impl GenConfig
-  def get(key), do: get_config() |> Map.fetch!(key)
+  def get(key), do: get_all() |> Map.fetch!(key)
+
+  # Parses as integer if parsable. If not, returns original value.
+  defp parse_int(v) when is_binary(v) do
+    case Integer.parse(v) do
+      {i, ""} -> i
+      _ -> v
+    end
+  end
+
+  defp parse_int(v), do: v
 end
