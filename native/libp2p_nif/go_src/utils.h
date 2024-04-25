4efec4f92a5e9ed0984d7b6fbd3bfe8ce094823c
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-04-25 11:38:18 -0300

    chore: remove libp2p NIF source code (#1041)

--- native/libp2p_nif/go_src/utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdint.h> // for uintptr_t
-#include <stdbool.h>
-
-typedef bool (*send_message1_t)(void *pid_bytes, void *arg1);
-
-bool run_callback1(send_message1_t send_message, void *pid_bytes, void *arg1);
