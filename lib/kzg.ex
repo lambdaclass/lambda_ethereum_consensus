3342290f235dea4b68b19bc0f5e918852ff298de
guha-rahul <52607971+guha-rahul@users.noreply.github.com>
2024-04-30 01:50:23 +0530

    refactor: move BLS and KZG specific types to their specific modules (#1057)

--- lib/kzg.ex
+++ lib/kzg.ex
@@ -4,35 +4,38 @@ defmodule Kzg do
   """
   use Rustler, otp_app: :lambda_ethereum_consensus, crate: "kzg_nif"
 
-  @spec blob_to_kzg_commitment(Types.blob()) :: {:ok, Types.kzg_commitment()} | {:error, binary()}
+  @type commitment :: <<_::384>>
+  @type proof :: <<_::768>>
+
+  @spec blob_to_kzg_commitment(Types.blob()) :: {:ok, commitment()} | {:error, binary()}
   def blob_to_kzg_commitment(_blob) do
     :erlang.nif_error(:nif_not_loaded)
   end
 
   @spec compute_kzg_proof(Types.blob(), Types.bytes32()) ::
-          {:ok, {Types.kzg_proof(), Types.bytes32()}} | {:error, binary()}
+          {:ok, {proof(), Types.bytes32()}} | {:error, binary()}
   def compute_kzg_proof(_blob, _z) do
     :erlang.nif_error(:nif_not_loaded)
   end
 
   @spec verify_kzg_proof(
-          Types.kzg_commitment(),
+          commitment(),
           Types.bytes32(),
           Types.bytes32(),
-          Types.kzg_proof()
+          proof()
         ) ::
           {:ok, boolean} | {:error, binary()}
   def verify_kzg_proof(_kzg_commitment, _z, _y, _kzg_proof) do
     :erlang.nif_error(:nif_not_loaded)
   end
 
-  @spec compute_blob_kzg_proof(Types.blob(), Types.kzg_commitment()) ::
-          {:ok, Types.kzg_proof()} | {:error, binary()}
+  @spec compute_blob_kzg_proof(Types.blob(), commitment()) ::
+          {:ok, proof()} | {:error, binary()}
   def compute_blob_kzg_proof(_blob, _kzg_commitment) do
     :erlang.nif_error(:nif_not_loaded)
   end
 
-  @spec verify_blob_kzg_proof(Types.blob(), Types.kzg_commitment(), Types.kzg_proof()) ::
+  @spec verify_blob_kzg_proof(Types.blob(), commitment(), proof()) ::
           {:ok, boolean} | {:error, binary()}
   def verify_blob_kzg_proof(_blob, _kzg_commitment, _kzg_proof) do
     :erlang.nif_error(:nif_not_loaded)
@@ -40,8 +43,8 @@ defmodule Kzg do
 
   @spec verify_blob_kzg_proof_batch(
           list(Types.blob()),
-          list(Types.kzg_commitment()),
-          list(Types.kzg_proof())
+          list(commitment()),
+          list(proof())
         ) ::
           {:ok, boolean} | {:error, binary()}
   def verify_blob_kzg_proof_batch(_blobs, _kzg_commitments, _kzg_proofs) do
@@ -54,8 +57,8 @@ defmodule Kzg do
 
   @spec blob_kzg_proof_batch_valid?(
           list(Types.blob()),
-          list(Types.kzg_commitment()),
-          list(Types.kzg_proof())
+          list(commitment()),
+          list(proof())
         ) :: boolean()
   def blob_kzg_proof_batch_valid?(blobs, kzg_commitments, kzg_proofs) do
     case verify_blob_kzg_proof_batch(blobs, kzg_commitments, kzg_proofs) do
