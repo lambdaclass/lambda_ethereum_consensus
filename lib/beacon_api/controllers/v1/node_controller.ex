0cb4188530acf91497fdd8f6bdff2dbf395df3de
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-05-03 11:44:01 -0300

    feat: add /eth/v1/node/identity endpoint

--- lib/beacon_api/controllers/v1/node_controller.ex
+++ lib/beacon_api/controllers/v1/node_controller.ex
@@ -10,11 +10,29 @@ defmodule BeaconApi.V1.NodeController do
   def open_api_operation(:health),
     do: ApiSpec.spec().paths["/eth/v1/node/health"].get
 
-  @spec health(Plug.Conn.t(), any) :: Plug.Conn.t()
-  def health(conn, params) do
-    # TODO: respond with syncing status if we're still syncing
-    _syncing_status = Map.get(params, :syncing_status, 206)
+  def open_api_operation(:identity),
+    do: ApiSpec.spec().paths["/eth/v1/node/identity"].get
 
-    send_resp(conn, 200, "")
+  @spec identity(Plug.Conn.t(), any) :: Plug.Conn.t()
+  def identity(conn, _params) do
+    conn
+    |> json(%{
+      data: %{
+        peer_id: "QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N",
+        enr:
+          "enr:-IS4QHCYrYZbAKWCBRlAy5zzaDZXJBGkcnh4MHcBFZntXNFrdvJjX04jRzjzCBOonrkTfj499SZuOh8R33Ls8RRcy5wBgmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQPKY0yuDUmstAHYpMa2_oxVtw0RW_QAdpzBQA8yWM0xOIN1ZHCCdl8",
+        p2p_addresses: [
+          "/ip4/7.7.7.7/tcp/4242/p2p/QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N"
+        ],
+        discovery_addresses: [
+          "/ip4/7.7.7.7/udp/30303/p2p/QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N"
+        ],
+        metadata: %{
+          seq_number: "1",
+          attnets: "0x0000000000000000",
+          syncnets: "0x0f"
+        }
+      }
+    })
   end
 end
