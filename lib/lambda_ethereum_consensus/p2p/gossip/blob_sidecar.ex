4417bea2d655731d3af2d56ff63d19a87b5f2820
avilagaston9 <gavila@fi.uba.ar>
2024-06-04 12:41:56 -0300

    Remove indirection in function handle_gossip_message

--- lib/lambda_ethereum_consensus/p2p/gossip/blob_sidecar.ex
+++ lib/lambda_ethereum_consensus/p2p/gossip/blob_sidecar.ex
@@ -13,8 +13,17 @@ defmodule LambdaEthereumConsensus.P2P.Gossip.BlobSideCar do
 
   @impl true
   def handle_gossip_message(_topic, msg_id, message) do
-    Task.start_link(__MODULE__, :handle_blob, [msg_id, message])
-    :ok
+    with {:ok, uncompressed} <- :snappyer.decompress(message),
+         {:ok, %Types.BlobSidecar{index: blob_index} = blob} <-
+           Ssz.from_ssz(uncompressed, Types.BlobSidecar) do
+      Logger.info("[Gossip] Blob sidecar received, with index #{blob_index}")
+      BlobDb.store_blob(blob)
+      Libp2pPort.validate_message(msg_id, :accept)
+    else
+      {:error, reason} ->
+        Logger.warning("[Gossip] Blob rejected, reason: #{inspect(reason)}")
+        Libp2pPort.validate_message(msg_id, :reject)
+    end
   end
 
   @spec join_topics() :: :ok
@@ -48,19 +57,5 @@ defmodule LambdaEthereumConsensus.P2P.Gossip.BlobSideCar do
     Enum.map(0..(ChainSpec.get("BLOB_SIDECAR_SUBNET_COUNT") - 1), fn i ->
       "/eth2/#{fork_context}/blob_sidecar_#{i}/ssz_snappy"
     end)
-  end
-
-  def handle_blob(msg_id, message) do
-    with {:ok, uncompressed} <- :snappyer.decompress(message),
-         {:ok, %Types.BlobSidecar{index: blob_index} = blob} <-
-           Ssz.from_ssz(uncompressed, Types.BlobSidecar) do
-      Logger.debug("[Gossip] Blob sidecar received, with index #{blob_index}")
-      BlobDb.store_blob(blob)
-      Libp2pPort.validate_message(msg_id, :accept)
-    else
-      {:error, reason} ->
-        Logger.warning("[Gossip] Blob rejected, reason: #{inspect(reason)}")
-        Libp2pPort.validate_message(msg_id, :reject)
-    end
   end
 end
