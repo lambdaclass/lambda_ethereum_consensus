f8cd66e91b8ad55ee4600275a5eddf24ec7e57b5
avilagaston9 <gavila@fi.uba.ar>
2024-06-19 11:18:13 -0300

    test: add bit_field tests with more than 8 bits

--- test/unit/bit_field_test.exs
+++ test/unit/bit_field_test.exs
@@ -8,11 +8,23 @@ defmodule Unit.BitFieldTest do
 
     assert BitField.bitwise_or(<<0b1010_1001::size(8)>>, <<0b0000_0010::size(8)>>) ==
              <<0b1010_1011::size(8)>>
+
+    assert BitField.bitwise_or(<<0b0000000000000000::size(16)>>, <<0b1111111111111111::size(16)>>) ==
+             <<0b1111111111111111::size(16)>>
+
+    assert BitField.bitwise_or(<<0b1100110011001100::size(16)>>, <<0b0011001100110011::size(16)>>) ==
+             <<0b1111111111111111::size(16)>>
   end
 
   test "bitwise OR with sizes not multiple of 8" do
     assert BitField.bitwise_or(<<0b101::size(3)>>, <<0b010::size(3)>>) == <<0b111::size(3)>>
     assert BitField.bitwise_or(<<0b1111::size(7)>>, <<0b0001::size(7)>>) == <<0b1111::size(7)>>
+
+    assert BitField.bitwise_or(<<0b1010101010101::size(13)>>, <<0b0101010101010::size(13)>>) ==
+             <<0b1111111111111::size(13)>>
+
+    assert BitField.bitwise_or(<<0b1111000000001::size(13)>>, <<0b0000111111110::size(13)>>) ==
+             <<0b1111111111111::size(13)>>
   end
 
   test "bitwise OR of all-zero and all-one bitfields" do
