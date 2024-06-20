c84f42c0d7002050629db30039ecfa6ae5dc1442
Tomás Arjovsky <t.arjovsky@gmail.com>
2024-06-20 18:28:31 +0200

    add metrics in the kv_schema module for get, put, delete, fold operations

--- lib/lambda_ethereum_consensus/store/kv_schema.ex
+++ lib/lambda_ethereum_consensus/store/kv_schema.ex
@@ -19,41 +19,54 @@ defmodule LambdaEthereumConsensus.Store.KvSchema do
 
   defmacro __using__(prefix: prefix) do
     quote do
+      # These types are just for documentation reasons.
+      @type key :: term()
+      @type value :: term()
+      @type acc :: term()
+
       @prefix unquote(prefix)
       @behaviour LambdaEthereumConsensus.Store.KvSchema
 
-      @spec get(term()) :: {:ok, term()} | {:error, binary()} | :not_found
+      @spec get(key()) :: {:ok, value()} | {:error, binary()} | :not_found
       def get(key) do
-        with {:ok, encoded_key} <- encode_key(key),
-             {:ok, encoded_value} <- Db.get(@prefix <> encoded_key) do
-          decode_value(encoded_value)
-        end
+        db_span("get", fn ->
+          with {:ok, encoded_key} <- encode_key(key),
+               {:ok, encoded_value} <- Db.get(@prefix <> encoded_key) do
+            decode_value(encoded_value)
+          end
+        end)
       end
 
-      @spec put(term(), term()) :: :ok | {:error, binary()}
+      @spec put(key(), value()) :: :ok | {:error, binary()}
       def put(key, value) do
-        with {:ok, encoded_key} <- encode_key(key),
-             {:ok, encoded_value} <- encode_value(value) do
-          Db.put(@prefix <> encoded_key, encoded_value)
-        end
+        db_span("put", fn ->
+          with {:ok, encoded_key} <- encode_key(key),
+               {:ok, encoded_value} <- encode_value(value) do
+            Db.put(@prefix <> encoded_key, encoded_value)
+          end
+        end)
       end
 
-      @spec delete(term()) :: :ok | {:error, binary()}
+      @spec delete(key()) :: :ok | {:error, binary()}
       def delete(key) do
-        with {:ok, encoded_key} <- encode_key(key) do
-          Db.delete(@prefix <> encoded_key)
-        end
+        db_span("delete", fn ->
+          with {:ok, encoded_key} <- encode_key(key) do
+            Db.delete(@prefix <> encoded_key)
+          end
+        end)
       end
 
-      @spec fold(term(), term(), (term(), term() -> term())) :: term()
+      @spec fold(key(), acc(), (key(), acc() -> acc())) :: acc()
       def fold(start_key, starting_value, f) do
-        with {:ok, it} <- Db.iterate(),
-             {:ok, encoded_start} <- encode_key(start_key),
-             {:ok, @prefix <> _, _} <- Exleveldb.iterator_move(it, encoded_start) do
-          res = iterate(it, starting_value, f)
-          Exleveldb.iterator_close(it)
-          {:ok, res}
-        end
+        db_span("fold", fn ->
+          with {:ok, it} <- Db.iterate(),
+               {:ok, encoded_start} <- encode_key(start_key),
+               {:ok, @prefix <> _, _} <- Exleveldb.iterator_move(it, encoded_start) do
+            res = iterate(it, starting_value, f)
+            Exleveldb.iterator_close(it)
+            {:ok, res}
+          end
+        end)
       end
 
       defp iterate(it, acc, f) do
@@ -62,6 +75,10 @@ defmodule LambdaEthereumConsensus.Store.KvSchema do
           {:ok, @prefix <> k, v} -> iterate(it, f.(decode_key(k), acc), f)
           _ -> acc
         end
+      end
+
+      defp db_span(action, f) do
+        :telemetry.span([:db, :latency], %{}, fn -> {f.(), %{module: @prefix, action: action}} end)
       end
     end
   end
