45452a94ddff8635f9e6696e4f13d9dd31896b96
avilagaston9 <gavila@fi.uba.ar>
2024-07-30 13:23:24 -0300

    refactor: adds metrics_port

--- config/runtime.exs
+++ config/runtime.exs
@@ -14,6 +14,7 @@ switches = [
   datadir: :string,
   testnet_dir: :string,
   metrics: :boolean,
+  metrics_port: :integer,
   log_file: :string,
   beacon_api: :boolean,
   beacon_api_port: :integer,
@@ -42,7 +43,8 @@ checkpoint_sync_url = Keyword.get(args, :checkpoint_sync_url)
 execution_endpoint = Keyword.get(args, :execution_endpoint, "http://localhost:8551")
 jwt_path = Keyword.get(args, :execution_jwt)
 testnet_dir = Keyword.get(args, :testnet_dir)
-disable_metrics = not Keyword.get(args, :metrics, false)
+enable_metrics = Keyword.get(args, :metrics, false)
+metrics_port = Keyword.get(args, :metrics_port, if(enable_metrics, do: 9568, else: nil))
 beacon_api_port = Keyword.get(args, :beacon_api_port, nil)
 enable_beacon_api = Keyword.get(args, :beacon_api, not is_nil(beacon_api_port))
 listen_addresses = Keyword.get_values(args, :listen_address)
@@ -187,9 +189,10 @@ block_time_ms =
 # Metrics
 
 config :lambda_ethereum_consensus, LambdaEthereumConsensus.PromEx,
-  disabled: disable_metrics,
+  disabled: not enable_metrics,
   metrics_server: [
-    port: 9568,
+    port: metrics_port,
+    protocol: :http,
     auth_strategy: :none
   ],
   grafana: [
