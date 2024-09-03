a2f6b97957c632af794d2eee61be747783050c20
Leo Bazan <leo.bazan.x@gmail.com>
2024-09-03 18:38:34 -0300

    refactor: Wrap Exleveldb and :eleveldb functions in Db

--- lib/lambda_ethereum_consensus/store/db.ex
+++ lib/lambda_ethereum_consensus/store/db.ex
@@ -2,13 +2,14 @@ defmodule LambdaEthereumConsensus.Store.Db do
   @moduledoc """
   Module that handles the key-value store.
   """
-
   require Logger
-
   # TODO: replace GenServer with :ets
   use GenServer
 
   @registered_name __MODULE__
+  @actions_with_db_ref [:put, :delete, :get, :iterator]
+  @actions_without_db_ref [:iterator_close, :iterator_move]
+  @raw_actions_with_db_ref [:status]
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: @registered_name)
@@ -29,6 +30,12 @@ defmodule LambdaEthereumConsensus.Store.Db do
     perform(:get, [key])
   end
 
+  @spec size() :: non_neg_integer()
+  def size() do
+    {:ok, size} = perform(:status, ["leveldb.total-bytes"])
+    String.to_integer(size)
+  end
+
   @spec iterate() :: {:ok, :eleveldb.itr_ref()} | {:error, any()}
   def iterate() do
     perform(:iterator, [[]])
@@ -39,10 +46,14 @@ defmodule LambdaEthereumConsensus.Store.Db do
     perform(:iterator, [[], :keys_only])
   end
 
-  @spec size() :: non_neg_integer()
-  def size() do
-    {:ok, size} = :eleveldb.status(ref(), "leveldb.total-bytes")
-    String.to_integer(size)
+  @spec iterator_close(binary) :: :ok
+  def iterator_close(iter_ref) do
+    perform(:iterator_close, [iter_ref])
+  end
+
+  @spec iterator_move(binary, Atom) :: {:ok, Atom, Atom} | {:error, any()}
+  def iterator_move(iter_ref, action) do
+    perform(:iterator_move, [iter_ref, action])
   end
 
   @impl true
@@ -64,13 +75,30 @@ defmodule LambdaEthereumConsensus.Store.Db do
   @impl true
   def handle_call(:get_ref, _from, %{ref: ref} = state), do: {:reply, ref, state}
 
+  # ---------------------------------------------------------------------------
+  # Private
+  # ---------------------------------------------------------------------------
+
   defp get_dir() do
     Application.fetch_env!(:lambda_ethereum_consensus, __MODULE__)
     |> Keyword.fetch!(:dir)
   end
 
-  defp perform(name, args) do
+  defp perform(name, args) when name in @actions_with_db_ref do
     apply(Exleveldb, name, [ref() | args])
+  end
+
+  defp perform(name, args) when name in @actions_without_db_ref do
+    apply(Exleveldb, name, args)
+  end
+
+  defp perform(name, args) when name in @raw_actions_with_db_ref do
+    apply(:eleveldb, name, [ref() | args])
+  end
+
+  defp perform(name, args) do
+    Logger.error("[Db] Failed to perform #{name} with args #{inspect(args)}")
+    {:error, "unknown operation"}
   end
 
   defp ref() do
