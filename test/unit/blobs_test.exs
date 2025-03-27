6c926376afec20ed70ff2aee707c6de7475dee1b
Leandro Serra <leandro.serra@lambdaclass.com>
2025-03-27 13:56:00 -0300

    blobs unit tests

---
+++ test/unit/blobs_test.exs
@@ -0,0 +1,73 @@
+defmodule Unit.BlobsTest do
+  use ExUnit.Case
+  alias Fixtures.Block
+  alias LambdaEthereumConsensus.Store.BlobDb
+  alias LambdaEthereumConsensus.Store.Blobs
+  alias SpecTestUtils
+  alias Types.BlobSidecar
+  alias Types.BlockInfo
+
+  setup %{tmp_dir: tmp_dir} do
+    start_link_supervised!({LambdaEthereumConsensus.Store.Db, dir: tmp_dir})
+    start_link_supervised!(LambdaEthereumConsensus.Store.Blocks)
+    :ok
+  end
+
+  defp new_block_info() do
+    Block.signed_beacon_block() |> BlockInfo.from_block()
+  end
+
+  describe "Blobs unit tests" do
+    @tag :tmp_dir
+    test "Basic blobs saving and loading" do
+      # Blob sidecar from spec test
+      blob_sidecar =
+        SpecTestUtils.read_ssz_from_file!(
+          "test/fixtures/validator/proposer/blob_sidecar.ssz_snappy",
+          BlobSidecar
+        )
+
+      Blobs.process_blobs(nil, {:ok, [blob_sidecar]})
+      block_root = Ssz.hash_tree_root!(blob_sidecar.signed_block_header.message)
+      index = blob_sidecar.index
+      {:ok, recovered_blob} = BlobDb.get_blob_sidecar(block_root, index)
+
+      assert(blob_sidecar == recovered_blob)
+    end
+
+    @tag :tmp_dir
+    test "One missing blob from block, then add, then no missing blobs" do
+      blob_sidecar =
+        SpecTestUtils.read_ssz_from_file!(
+          "test/fixtures/validator/proposer/blob_sidecar.ssz_snappy",
+          BlobSidecar
+        )
+
+      blob_sidecar = %BlobSidecar{blob_sidecar | index: 0}
+
+      # Create random block info
+      block_info = new_block_info()
+      # add blob_sidecar kzg_commitment to the block_info
+      block_info =
+        put_in(
+          block_info.signed_block.message.body.blob_kzg_commitments,
+          [blob_sidecar.kzg_commitment]
+        )
+
+      # change block root to the one from the blob
+      block_info = %BlockInfo{
+        block_info
+        | root: Ssz.hash_tree_root!(blob_sidecar.signed_block_header.message)
+      }
+
+      # check that the blob is detetected as missing
+      missing = Blobs.missing_blobs(block_info)
+      assert(length(missing) == 1)
+      # add blob to db
+      Blobs.add_blob(nil, blob_sidecar)
+      # check that the blob is not missing
+      missing = Blobs.missing_blobs(block_info)
+      assert(Enum.empty?(missing))
+    end
+  end
+end
