a73c061098a2dd43c59a898dc7e332302a3f439c
Leandro Serra <leandro.serra@lambdaclass.com>
2025-04-09 14:13:54 -0300

    feat apply_deposit Electra changes

--- lib/lambda_ethereum_consensus/state_transition/mutators.ex
+++ lib/lambda_ethereum_consensus/state_transition/mutators.ex
@@ -5,6 +5,8 @@ defmodule LambdaEthereumConsensus.StateTransition.Mutators do
   alias LambdaEthereumConsensus.StateTransition.Accessors
   alias LambdaEthereumConsensus.StateTransition.Misc
   alias Types.BeaconState
+  alias Types.DepositMessage
+  alias Types.PendingDeposit
   alias Types.Validator
 
   @doc """
@@ -122,27 +124,36 @@ defmodule LambdaEthereumConsensus.StateTransition.Mutators do
           Types.bls_signature()
         ) :: {:ok, BeaconState.t()} | {:error, String.t()}
   def apply_deposit(state, pubkey, withdrawal_credentials, amount, signature) do
-    case Enum.find_index(state.validators, fn validator -> validator.pubkey == pubkey end) do
-      index when is_number(index) ->
-        {:ok, BeaconState.increase_balance(state, index, amount)}
+    deposit = %PendingDeposit{
+      pubkey: pubkey,
+      withdrawal_credentials: withdrawal_credentials,
+      amount: amount,
+      signature: signature,
+      slot: Constants.genesis_slot()
+    }
 
-      _ ->
-        deposit_message = %Types.DepositMessage{
-          pubkey: pubkey,
-          withdrawal_credentials: withdrawal_credentials,
-          amount: amount
-        }
-
-        domain = Misc.compute_domain(Constants.domain_deposit())
-
-        signing_root = Misc.compute_signing_root(deposit_message, domain)
-
-        if Bls.valid?(pubkey, signing_root, signature) do
-          apply_initial_deposit(state, pubkey, withdrawal_credentials, amount)
+    pending_deposits =
+      if !Enum.member?(state.validators, fn validator -> validator.pubkey == pubkey end) do
+        if DepositMessage.valid_deposit_signature?(
+             pubkey,
+             withdrawal_credentials,
+             amount,
+             signature
+           ) do
+          {:ok, state} = apply_initial_deposit(state, pubkey, withdrawal_credentials, amount)
+          state.pending_deposits ++ [deposit]
         else
-          {:ok, state}
+          state.pending_deposits
         end
-    end
+      else
+        state.pending_deposits ++ [deposit]
+      end
+
+    {:ok,
+     %BeaconState{
+       state
+       | pending_deposits: pending_deposits
+     }}
   end
 
   defp apply_initial_deposit(%BeaconState{} = state, pubkey, withdrawal_credentials, amount) do
