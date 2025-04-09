12ed2d3b0e336ce961a16044eae053db9846b699
Leandro Serra <leandro.serra@lambdaclass.com>
2025-04-09 14:12:58 -0300

    feat get_validator_from_deposit Electra changes

--- lib/types/beacon_chain/deposit.ex
+++ lib/types/beacon_chain/deposit.ex
@@ -22,23 +22,28 @@ defmodule Types.Deposit do
   @spec get_validator_from_deposit(Types.bls_pubkey(), Types.bytes32(), Types.uint64()) ::
           Types.Validator.t()
   def get_validator_from_deposit(pubkey, withdrawal_credentials, amount) do
-    effective_balance =
-      min(
-        amount - rem(amount, ChainSpec.get("EFFECTIVE_BALANCE_INCREMENT")),
-        ChainSpec.get("MAX_EFFECTIVE_BALANCE")
-      )
-
     far_future_epoch = Constants.far_future_epoch()
 
-    %Types.Validator{
+    validator = %Types.Validator{
       pubkey: pubkey,
       withdrawal_credentials: withdrawal_credentials,
       activation_eligibility_epoch: far_future_epoch,
       activation_epoch: far_future_epoch,
       exit_epoch: far_future_epoch,
       withdrawable_epoch: far_future_epoch,
-      effective_balance: effective_balance,
+      effective_balance: 0,
       slashed: false
+    }
+
+    effective_balance =
+      min(
+        amount - rem(amount, ChainSpec.get("EFFECTIVE_BALANCE_INCREMENT")),
+        Types.Validator.get_max_effective_balance(validator)
+      )
+
+    %Types.Validator{
+      validator
+      | effective_balance: effective_balance
     }
   end
 
