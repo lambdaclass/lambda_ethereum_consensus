2ae6bc92a6b916a9fcec88874d105b0166210554
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2026-03-05 13:41:43 -0300

    fix: add snappy

--- flake.nix
+++ flake.nix
@@ -35,6 +35,7 @@
               pkgs.glibcLocales
               pkgs.protobuf3_24
               pkgs.cmake
+              pkgs.snappy
             ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [ libiconv ])
               ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
               CoreFoundation
